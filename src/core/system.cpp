e0086156ca0b051eab0b606f0c93fc28bef2debc
Connor McLaughlin <stenzek@gmail.com>
2020-01-08 13:34:21 +1000

    System: Compile fix for Linux

--- src/core/system.cpp
+++ src/core/system.cpp
@@ -207,9 +207,6 @@ bool System::CreateGPU()
     case GPURenderer::HardwareD3D11:
       m_gpu = GPU::CreateHardwareD3D11Renderer();
       break;
-#else
-    case GPURenderer::HardwareD3D11:
-      break;
 #endif
 
     case GPURenderer::Software:
