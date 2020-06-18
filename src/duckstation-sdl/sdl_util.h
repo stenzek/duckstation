d168947ae4a0ec27ceee2cf1907cc2c761898df4
Connor McLaughlin <stenzek@gmail.com>
2020-06-19 00:18:51 +1000

    SDL: Support Vulkan

---
+++ src/duckstation-sdl/sdl_util.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "common/types.h"
+#include "common/window_info.h"
+#include <optional>
+
+struct SDL_Window;
+
+namespace SDLUtil {
+std::optional<WindowInfo> GetWindowInfoForSDLWindow(SDL_Window* window);
+}
