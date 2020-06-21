73cdd676adcce8fbfc50f60aa74c5a375a35ae29
Connor McLaughlin <stenzek@gmail.com>
2020-06-21 16:24:06 +1000

    GPU/Vulkan: Fix incorrect srcfactor selection

--- src/core/gpu_hw_vulkan.cpp
+++ src/core/gpu_hw_vulkan.cpp
@@ -658,7 +658,7 @@ bool GPU_HW_Vulkan::CompilePipelines()
                 {
                   gpbuilder.SetBlendAttachment(
                     0, true, VK_BLEND_FACTOR_ONE,
-                    m_supports_dual_source_blend ? VK_BLEND_FACTOR_SRC_ALPHA : VK_BLEND_FACTOR_SRC1_ALPHA,
+                    m_supports_dual_source_blend ? VK_BLEND_FACTOR_SRC1_ALPHA : VK_BLEND_FACTOR_SRC_ALPHA,
                     (static_cast<TransparencyMode>(transparency_mode) == TransparencyMode::BackgroundMinusForeground) ?
                       VK_BLEND_OP_REVERSE_SUBTRACT :
                       VK_BLEND_OP_ADD,
