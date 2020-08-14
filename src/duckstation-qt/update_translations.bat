ce9c582999623cbed0fba5a8344b4f866615679f
phoe-nix <wanwei1116@sina.com>
2020-08-15 02:00:36 +0800

    update zh translate

--- src/duckstation-qt/update_translations.bat
+++ src/duckstation-qt/update_translations.bat
@@ -1,5 +1,6 @@
 ..\..\dep\msvc\qt\5.15.0\msvc2017_64\bin\lupdate.exe ./ -ts translations\duckstation-qt_de.ts
 ..\..\dep\msvc\qt\5.15.0\msvc2017_64\bin\lupdate.exe ./ -ts translations\duckstation-qt_he.ts
+..\..\dep\msvc\qt\5.15.0\msvc2017_64\bin\lupdate.exe ./ -ts translations\duckstation-qt_zh-cn.ts
 ..\..\dep\msvc\qt\5.15.0\msvc2017_64\bin\lupdate.exe ./ -ts translations\duckstation-qt_pt-br.ts
 ..\..\dep\msvc\qt\5.15.0\msvc2017_64\bin\lupdate.exe ./ -ts translations\duckstation-qt_pt-pt.ts
 pause
