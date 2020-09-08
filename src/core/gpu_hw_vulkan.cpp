2cecb8bbdbafe0c42e9d57d971f812f7d31f6b6a
Connor McLaughlin <stenzek@gmail.com>
2020-09-08 11:40:54 +1000

    GPU/Vulkan: End render pass in ClearDisplay()
    Fixes crash on Mesa/radv.

--- src/core/gpu_hw_vulkan.cpp
+++ src/core/gpu_hw_vulkan.cpp
@@ -909,6 +909,7 @@ void GPU_HW_Vulkan::SetScissorFromDrawingArea()
 void GPU_HW_Vulkan::ClearDisplay()
 {
   GPU_HW::ClearDisplay();
+  EndRenderPass();
 
   VkCommandBuffer cmdbuf = g_vulkan_context->GetCurrentCommandBuffer();
   m_display_texture.TransitionToLayout(cmdbuf, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
