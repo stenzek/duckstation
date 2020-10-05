b2dc08bd8c0f28580b9b6ff3b1b7ca4c3afe1389
Connor McLaughlin <stenzek@gmail.com>
2020-10-05 15:22:47 +1000

    Update update-and-edit-language.bat

--- src/duckstation-qt/translations/update-and-edit-language.bat
+++ src/duckstation-qt/translations/update-and-edit-language.bat
@@ -3,7 +3,7 @@
 if not defined lang (echo Please set your language first & pause & exit)
 
 set "linguist=..\..\..\dep\msvc\qt\5.15.0\msvc2017_64\bin"
-set context=..\
+set context=../ ../../core/ ../../frontend-common/ -tr-function-alias translate+=TranslateString -tr-function-alias translate+=TranslateStdString -tr-function-alias QT_TRANSLATE_NOOP+=TRANSLATABLE
 
 "%linguist%\lupdate.exe" %context% -ts duckstation-qt_%lang%.ts
 pause
