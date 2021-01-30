dc0eb4cb04018f3b3e5b52016c6afba5b6e2f2bd
Connor McLaughlin <stenzek@gmail.com>
2021-01-30 22:22:46 +1000

    Replace duckstation-sdl with stub directing users to duckstation-qt

--- src/duckstation-sdl/main.cpp
+++ src/duckstation-sdl/main.cpp
@@ -1,48 +1,27 @@
-#include "common/assert.h"
-#include "common/log.h"
-#include "core/system.h"
-#include "frontend-common/sdl_initializer.h"
-#include "sdl_host_interface.h"
-#include <SDL.h>
+#ifdef _WIN32
+#include "common/windows_headers.h"
+#endif
+
 #include <cstdio>
 #include <cstdlib>
 
+static const char message[] = "The DuckStation SDL frontend has been removed and replaced with the Fullscreen UI.\n\n"
+                              "Please use duckstation-qt instead.";
+
+#ifdef _WIN32
+
+int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+  MessageBoxA(nullptr, message, "DuckStation", MB_OK | MB_ICONERROR);
+  return -1;
+}
+
+#else
+
 int main(int argc, char* argv[])
 {
-  FrontendCommon::EnsureSDLInitialized();
-
-  std::unique_ptr<SDLHostInterface> host_interface = SDLHostInterface::Create();
-  std::unique_ptr<SystemBootParameters> boot_params;
-  if (!host_interface->ParseCommandLineParameters(argc, argv, &boot_params))
-  {
-    SDL_Quit();
-    return EXIT_FAILURE;
-  }
-
-  if (!host_interface->Initialize())
-  {
-    host_interface->Shutdown();
-    SDL_Quit();
-    return EXIT_FAILURE;
-  }
-
-  if (boot_params)
-  {
-    if (!host_interface->BootSystem(*boot_params) && host_interface->InBatchMode())
-    {
-      host_interface->Shutdown();
-      host_interface.reset();
-      SDL_Quit();
-      return EXIT_FAILURE;
-    }
-
-    boot_params.reset();
-  }
-
-  host_interface->Run();
-  host_interface->Shutdown();
-  host_interface.reset();
-
-  SDL_Quit();
-  return EXIT_SUCCESS;
+  std::fputs(message, stderr);
+  return -1;
 }
+
+#endif
