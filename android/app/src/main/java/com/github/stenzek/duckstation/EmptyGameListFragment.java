90e0ff33dbe4ee72e9024697dadd6d837326828b
Connor McLaughlin <stenzek@gmail.com>
2021-04-17 21:22:51 +1000

    Android: Improve text in empty game list fragment

--- android/app/src/main/java/com/github/stenzek/duckstation/EmptyGameListFragment.java
+++ android/app/src/main/java/com/github/stenzek/duckstation/EmptyGameListFragment.java
@@ -10,7 +10,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class EmptyGameListFragment extends Fragment {
-    private static final String SUPPORTED_FORMATS_STRING = ".cue, .iso, .img, .ecm, .mds, .chd, .pbp (unencrypted)";
+    private static final String SUPPORTED_FORMATS_STRING =
+            ".cue (Cue Sheets)\n" +
+            ".iso/.img (Single Track Image)\n" +
+            ".ecm (Error Code Modeling Image)\n" +
+            ".mds (Media Descriptor Sidecar)\n" +
+            ".chd (Compressed Hunks of Data)\n" +
+            ".pbp (PlayStation Portable, Only Encrypted)";
 
     private MainActivity parent;
 
@@ -23,9 +29,8 @@ public class EmptyGameListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        ((TextView)view.findViewById(R.id.supported_formats)).setText(
+        ((TextView) view.findViewById(R.id.supported_formats)).setText(
                 getString(R.string.main_activity_empty_game_list_supported_formats, SUPPORTED_FORMATS_STRING));
-        ((Button)view.findViewById(R.id.add_game_directory)).setOnClickListener(v -> parent.startAddGameDirectory());
-        ((Button)view.findViewById(R.id.start_file)).setOnClickListener(v -> parent.startStartFile());
+        ((Button) view.findViewById(R.id.add_game_directory)).setOnClickListener(v -> parent.startAddGameDirectory());
     }
 }
