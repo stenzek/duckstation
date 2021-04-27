e6383286d9c4bf34ba6253ccaeffb536319ca3a5
Connor McLaughlin <stenzek@gmail.com>
2021-04-28 02:36:19 +1000

    D3D11HostDisplay: Make screenshots/display rendering opaque

--- src/frontend-common/display_ps.hlsl
+++ src/frontend-common/display_ps.hlsl
@@ -4,5 +4,5 @@ SamplerState samp0_ss : register(s0);
 void main(in float2 v_tex0 : TEXCOORD0,
           out float4 o_col0 : SV_Target)
 {
-  o_col0 = samp0.Sample(samp0_ss, v_tex0);
+  o_col0 = float4(samp0.Sample(samp0_ss, v_tex0).rgb, 1.0);
 }
