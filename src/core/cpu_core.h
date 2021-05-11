acda42be1625b985351f9f33df9bb1094906262b
Connor McLaughlin <stenzek@gmail.com>
2021-05-12 02:26:58 +1000

    CPU/Recompiler: Get rid of non-constant offsetofs

--- src/core/cpu_core.h
+++ src/core/cpu_core.h
@@ -84,6 +84,9 @@ struct State
   std::array<u8, DCACHE_SIZE> dcache = {};
   std::array<u32, ICACHE_LINES> icache_tags = {};
   std::array<u8, ICACHE_SIZE> icache_data = {};
+
+  static constexpr u32 GPRRegisterOffset(u32 index) { return offsetof(State, regs.r) + (sizeof(u32) * index); }
+  static constexpr u32 GTERegisterOffset(u32 index) { return offsetof(State, gte_regs.r32) + (sizeof(u32) * index); }
 };
 
 extern State g_state;
