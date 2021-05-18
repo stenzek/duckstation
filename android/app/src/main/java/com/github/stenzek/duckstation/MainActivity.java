9784c9d93022ece334c912e9394c0b6104b6bf4d
Connor McLaughlin <stenzek@gmail.com>
2021-05-18 17:36:43 +1000

    Android: Only use scoped storage for scanning on 11+

--- android/app/src/main/java/com/github/stenzek/duckstation/MainActivity.java
+++ android/app/src/main/java/com/github/stenzek/duckstation/MainActivity.java
@@ -272,9 +272,9 @@ public class MainActivity extends AppCompatActivity {
                 if (resultCode != RESULT_OK || data.getData() == null)
                     return;
 
-                // Use legacy storage on devices older than Android 9... apparently some of them
+                // Use legacy storage on devices older than Android 11... apparently some of them
                 // have broken storage access framework....
-                if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.P) {
+                if (android.os.Build.VERSION.SDK_INT < 30) {
                     final String path = FileHelper.getFullPathFromTreeUri(data.getData(), this);
                     if (path != null) {
                         GameDirectoriesActivity.addSearchDirectory(this, path, true);
