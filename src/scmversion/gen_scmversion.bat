e6b6ff6325302ca855454d6bc4cb120e9d33361c
Connor McLaughlin <stenzek@gmail.com>
2021-10-23 23:50:11 +1000

    Revert "Add version number to the Windows executable"
    This reverts commit b0cd59e5fb421c4f2d11e34b241f1b7e4857004a.

--- src/scmversion/gen_scmversion.bat
+++ src/scmversion/gen_scmversion.bat
@@ -23,11 +23,5 @@ ECHO const char* g_scm_tag_str = "%TAG%";
 ECHO const char* g_scm_date_str = "%CDATE%";
 )>%VERSIONFILE%
 
-powershell -Command "(gc ..\duckstation-qt\duckstation-qt.rc) -replace '1,0,0,1', '"%TAG:~0,1%","%TAG:~2,1%","%TAG:~4,4%",0' | Out-File -encoding ASCII ..\duckstation-qt\duckstation-qt.rc"
-powershell -Command "(gc ..\duckstation-qt\duckstation-qt.rc) -replace '1.0.0.1', '"%TAG:~0,1%"."%TAG:~2,1%"."%TAG:~4,4%"' | Out-File -encoding ASCII ..\duckstation-qt\duckstation-qt.rc"
-
-powershell -Command "(gc ..\duckstation-nogui\duckstation-nogui.rc) -replace '1,0,0,1', '"%TAG:~0,1%","%TAG:~2,1%","%TAG:~4,4%",0' | Out-File -encoding ASCII ..\duckstation-nogui\duckstation-nogui.rc
-powershell -Command "(gc ..\duckstation-nogui\duckstation-nogui.rc) -replace '1.0.0.1', '"%TAG:~0,1%"."%TAG:~2,1%"."%TAG:~4,4%"' | Out-File -encoding ASCII ..\duckstation-nogui\duckstation-nogui.rc"
-
 EXIT
 
