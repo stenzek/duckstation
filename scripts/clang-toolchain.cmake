0590a6098d6edb3995a7b5cac555e1a5ae737a12
Connor McLaughlin <stenzek@gmail.com>
2022-07-21 17:27:32 +1000

    CI: Fix up Linux/Mac build and disable UWP (for now)

---
+++ scripts/clang-toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_COMPILER /usr/bin/clang-12)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++-12)
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
