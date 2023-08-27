674f9a5b1c419803d047da34510ea5c294cd2a89
Stenzek <stenzek@gmail.com>
2023-08-27 16:23:16 +1000

    Resources: Remove embedded crosshair image

--- src/core/resources.h
+++ src/core/resources.h
@@ -7,11 +7,6 @@
 
 namespace Resources {
 
-// Adapted from https://icons8.com/icon/15970/target
-constexpr u32 CROSSHAIR_IMAGE_WIDTH = 96;
-constexpr u32 CROSSHAIR_IMAGE_HEIGHT = 96;
-extern const std::array<u32, CROSSHAIR_IMAGE_WIDTH * CROSSHAIR_IMAGE_HEIGHT> CROSSHAIR_IMAGE_DATA;
-
 constexpr int WINDOW_ICON_WIDTH = 64;
 constexpr int WINDOW_ICON_HEIGHT = 64;
 extern unsigned int WINDOW_ICON_DATA[WINDOW_ICON_WIDTH * WINDOW_ICON_HEIGHT];
