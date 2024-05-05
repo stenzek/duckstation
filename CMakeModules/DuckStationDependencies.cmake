e4d940a476c39d90a1cffb92cf6c748147ae40fa
Stenzek <stenzek@gmail.com>
2024-05-05 13:26:08 +1000

    Deps: Update SDL2 to 2.30.3

--- CMakeModules/DuckStationDependencies.cmake
+++ CMakeModules/DuckStationDependencies.cmake
@@ -9,7 +9,7 @@ endif()
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_package(Threads REQUIRED)
 
-find_package(SDL2 2.30.2 REQUIRED)
+find_package(SDL2 2.30.3 REQUIRED)
 find_package(Zstd 1.5.5 REQUIRED)
 find_package(WebP REQUIRED) # v1.3.2, spews an error on Linux because no pkg-config.
 find_package(ZLIB REQUIRED) # 1.3, but Mac currently doesn't use it.
