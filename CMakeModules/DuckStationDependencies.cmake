00d2d862c2901b8519ea22a8615c25d9b85ca69a
Stenzek <stenzek@gmail.com>
2024-07-04 14:23:11 +1000

    Build: Use shared discord-rpc

--- CMakeModules/DuckStationDependencies.cmake
+++ CMakeModules/DuckStationDependencies.cmake
@@ -17,6 +17,7 @@ find_package(PNG 1.6.40 REQUIRED)
 find_package(JPEG REQUIRED) # No version because flatpak uses libjpeg-turbo.
 find_package(Freetype 2.11.1 REQUIRED)
 find_package(cpuinfo REQUIRED)
+find_package(DiscordRPC 3.4.0 REQUIRED)
 
 if(NOT WIN32)
   find_package(CURL REQUIRED)
