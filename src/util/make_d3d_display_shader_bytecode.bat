aab69b06b99d1a310a37142caf79c2b106c6333f
Stenzek <stenzek@gmail.com>
2024-09-01 23:17:17 +1000

    Misc: Remove unused files

--- src/util/make_d3d_display_shader_bytecode.bat
+++
@@ -1,3 +0,0 @@
-fxc /T vs_4_0 /E main /O3 /Fh display_vs.hlsl.h /Vn "static s_display_vs_bytecode" display_vs.hlsl
-fxc /T ps_4_0 /E main /O3 /Fh display_ps.hlsl.h /Vn "static s_display_ps_bytecode" display_ps.hlsl
-fxc /T ps_4_0 /E main /O3 /D ALPHA=1 /Fh display_ps_alpha.hlsl.h /Vn "static s_display_ps_alpha_bytecode" display_ps.hlsl
