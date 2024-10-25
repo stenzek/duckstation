9001e8482c2bb62ffaa0d3040fa68e987bba0b35
Bobby Smith <33353403+bslenul@users.noreply.github.com>
2024-10-25 11:59:54 +0200

    Settings: Re-enable "Start Fullscreen" per-game

--- src/duckstation-qt/interfacesettingswidget.cpp
+++ src/duckstation-qt/interfacesettingswidget.cpp
@@ -57,12 +57,7 @@ InterfaceSettingsWidget::InterfaceSettingsWidget(SettingsWindow* dialog, QWidget
   SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.pauseOnStart, "Main", "StartPaused", false);
   SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.saveStateOnExit, "Main", "SaveStateOnExit", true);
   SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.confirmPowerOff, "Main", "ConfirmPowerOff", true);
-
-  if (!m_dialog->isPerGameSettings())
-    SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.startFullscreen, "Main", "StartFullscreen", false);
-  else
-    SettingWidgetBinder::SetAvailability(m_ui.startFullscreen, false);
-
+  SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.startFullscreen, "Main", "StartFullscreen", false);
   SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.doubleClickTogglesFullscreen, "Main",
                                                "DoubleClickTogglesFullscreen", true);
   SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.renderToSeparateWindow, "Main", "RenderToSeparateWindow",
