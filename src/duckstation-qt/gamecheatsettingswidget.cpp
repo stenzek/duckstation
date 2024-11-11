e0fa361f7522acfb803e0ad69407634ac633f533
PugsyMAME <18102600+PugsyMAME@users.noreply.github.com>
2024-11-11 00:13:24 +0000

    Changed boolean parameters in last PR
    Changed boolean parameters in last PR as per discord

--- src/duckstation-qt/gamecheatsettingswidget.cpp
+++ src/duckstation-qt/gamecheatsettingswidget.cpp
@@ -529,7 +529,7 @@ void GameCheatSettingsWidget::importCodes(const std::string& file_contents)
   }
 
   reloadList();
-  g_emu_thread->reloadCheats(true, true, false, true);
+  g_emu_thread->reloadCheats(true, false, false, true);
 }
 
 void GameCheatSettingsWidget::newCode()
@@ -544,7 +544,7 @@ void GameCheatSettingsWidget::newCode()
 
   // no need to reload cheats yet, it's not active. just refresh the list
   reloadList();
-  g_emu_thread->reloadCheats(true, true, false, true);
+  g_emu_thread->reloadCheats(true, false, false, true);
 }
 
 void GameCheatSettingsWidget::editCode(const std::string_view code_name)
