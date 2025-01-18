d054d76a7694e202ec68a14da5cc70afc82735ad
Stenzek <stenzek@gmail.com>
2025-01-18 13:47:36 +1000

    FileSystem: Add Error to CopyFilePath()

--- src/common/file_system.h
+++ src/common/file_system.h
@@ -208,7 +208,7 @@ bool DeleteDirectory(const char* path, Error* error = nullptr);
 bool RecursiveDeleteDirectory(const char* path, Error* error = nullptr);
 
 /// Copies one file to another, optionally replacing it if it already exists.
-bool CopyFilePath(const char* source, const char* destination, bool replace);
+bool CopyFilePath(const char* source, const char* destination, bool replace, Error* error = nullptr);
 
 /// Returns the path to the current executable.
 std::string GetProgramPath();
