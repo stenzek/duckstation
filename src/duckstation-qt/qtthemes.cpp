e5cec056332e6287718546665674e86fde2c7da7
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2025-03-20 18:16:45 +0700

    Qt/BPM: Add Green Giant Theme (#3390)
    ＷＨＡＴ ＡＲＥ ＹＥ ＤＯＩＮＧ ＩＮ ＭＹ ＳＷＡＭＰ？？？

--- src/duckstation-qt/qtthemes.cpp
+++ src/duckstation-qt/qtthemes.cpp
@@ -202,6 +202,40 @@ void QtHost::SetStyleFromSettings()
     qApp->setPalette(darkPalette);
     qApp->setStyleSheet(QString());
   }
+	else if (theme == "greengiant")
+	{
+		// Custom palette by RedDevilus, Tame (Light/Washed out) Green as main color and Grayish Blue as complimentary.
+		// Alternative white theme.
+		qApp->setStyle(QStyleFactory::create("Fusion"));
+
+		const QColor black(25, 25, 25);
+		const QColor gray(111, 111, 111);
+		const QColor limerick(176, 196, 0);
+		const QColor brown(135, 100, 50);
+		const QColor pear(213, 222, 46);
+
+		QPalette greenGiantPalette;
+		greenGiantPalette.setColor(QPalette::Window, pear);
+		greenGiantPalette.setColor(QPalette::WindowText, black);
+		greenGiantPalette.setColor(QPalette::Base, limerick);
+		greenGiantPalette.setColor(QPalette::AlternateBase, brown.lighter());
+		greenGiantPalette.setColor(QPalette::ToolTipBase, brown);
+		greenGiantPalette.setColor(QPalette::ToolTipText, Qt::white);
+		greenGiantPalette.setColor(QPalette::Text, black);
+		greenGiantPalette.setColor(QPalette::Button, brown.lighter());
+		greenGiantPalette.setColor(QPalette::ButtonText, black.lighter());
+		greenGiantPalette.setColor(QPalette::Link, brown.lighter());
+		greenGiantPalette.setColor(QPalette::Highlight, brown);
+		greenGiantPalette.setColor(QPalette::HighlightedText, Qt::white);
+
+		greenGiantPalette.setColor(QPalette::Disabled, QPalette::ButtonText, gray);
+		greenGiantPalette.setColor(QPalette::Disabled, QPalette::WindowText, gray.darker());
+		greenGiantPalette.setColor(QPalette::Disabled, QPalette::Text, gray.darker());
+		greenGiantPalette.setColor(QPalette::Disabled, QPalette::Light, gray);
+
+		qApp->setPalette(greenGiantPalette);
+		qApp->setStyleSheet(QString());
+	}
   else if (theme == "pinkypals")
   {
     qApp->setStyle(QStyleFactory::create("Fusion"));
@@ -371,6 +405,8 @@ const char* Host::GetDefaultFullscreenUITheme()
     return "CobaltSky";
   else if (theme == "greymatter")
     return "GreyMatter";
+  else if (theme == "greengiant")
+    return "GreenGiant";
   else if (theme == "pinkypals")
     return "PinkyPals";
   else if (theme == "purplerain")
