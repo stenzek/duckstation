f6d10eb5d25efb25216c014896c91a79d174fc8f
Davide Pesavento <davidepesa@gmail.com>
2025-06-21 14:18:53 -0400

    Qt: Fix default button in InputBindingDialog

--- src/duckstation-qt/inputbindingdialog.cpp
+++ src/duckstation-qt/inputbindingdialog.cpp
@@ -25,7 +25,7 @@ InputBindingDialog::InputBindingDialog(SettingsInterface* sif, InputBindingInfo:
   m_ui.setupUi(this);
   m_ui.title->setText(
     tr("Bindings for %1 %2").arg(QString::fromStdString(m_section_name)).arg(QString::fromStdString(m_key_name)));
-  m_ui.buttonBox->button(QDialogButtonBox::Close)->setText(tr("Close"));
+  m_ui.buttonBox->button(QDialogButtonBox::Close)->setDefault(true);
 
   connect(m_ui.addBinding, &QPushButton::clicked, this, &InputBindingDialog::onAddBindingButtonClicked);
   connect(m_ui.removeBinding, &QPushButton::clicked, this, &InputBindingDialog::onRemoveBindingButtonClicked);
