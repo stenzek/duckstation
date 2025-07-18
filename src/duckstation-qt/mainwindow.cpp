f446e24b20a6f04140e06b156fa76acfc7e2950e
Davide Pesavento <davidepesa@gmail.com>
2025-07-18 18:44:02 -0400

    Qt: Don't exit fullscreen when switching to game list with RenderToSeparateWindow
    Amends 24a16db4376baccaf533fd4860c493a9c6d5f921

--- src/duckstation-qt/mainwindow.cpp
+++ src/duckstation-qt/mainwindow.cpp
@@ -2182,8 +2182,9 @@ bool MainWindow::shouldHideMainWindow() const
 
 void MainWindow::switchToGameListView()
 {
-  // Normally, we'd never end up here. But on MacOS, the global menu is accessible while fullscreen.
-  exitFullscreen(true);
+  if (QtHost::CanRenderToMainWindow())
+    // Normally, we'd never end up here. But on MacOS, the global menu is accessible while fullscreen.
+    exitFullscreen(true);
 
   if (!isShowingGameList())
   {
@@ -3033,8 +3034,7 @@ void MainWindow::onToolsCoverDownloaderTriggered()
 {
   // This can be invoked via big picture, so exit fullscreen.
   // Wait for the fullscreen request to actually go through, otherwise the downloader appears behind the main window.
-  if (isRenderingFullscreen())
-    exitFullscreen(true);
+  exitFullscreen(true);
 
   if (!m_cover_download_window)
   {
