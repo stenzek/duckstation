stenzek/duckstation

commit 76facb3da72dec88bc3ca0a2bf753e07f83a43f6

FullscreenUI: Fix default value for theme

src/core/fullscreen_ui.cpp
@@ -4161,7 +4161,7 @@ void FullscreenUI::DrawInterfaceSettingsPage()
 
   DrawStringListSetting(bsi, FSUI_ICONVSTR(ICON_FA_PAINTBRUSH, "Theme"),
                         FSUI_VSTR("Selects the color style to be used for Big Picture UI."), "UI", "FullscreenUITheme",
-                        "Dark", s_theme_names, s_theme_values, true,
+                        Host::GetDefaultFullscreenUITheme(), s_theme_names, s_theme_values, true,
                         [](std::string_view) { BeginTransition(LONG_TRANSITION_TIME, &FullscreenUI::SetTheme); });
 
   if (const TinyString current_value =

commit 00a2f5a2ce023a2f180479a28a7b99d56cfa53f1

FullscreenUI: Fix crash opening settings
Regression from 76facb3. Derp.

src/core/fullscreen_ui.cpp
@@ -4161,7 +4161,7 @@ void FullscreenUI::DrawInterfaceSettingsPage()
 
   DrawStringListSetting(bsi, FSUI_ICONVSTR(ICON_FA_PAINTBRUSH, "Theme"),
                         FSUI_VSTR("Selects the color style to be used for Big Picture UI."), "UI", "FullscreenUITheme",
-                        Host::GetDefaultFullscreenUITheme(), s_theme_names, s_theme_values, true,
+                        "", s_theme_names, s_theme_values, true,
                         [](std::string_view) { BeginTransition(LONG_TRANSITION_TIME, &FullscreenUI::SetTheme); });
 
   if (const TinyString current_value =