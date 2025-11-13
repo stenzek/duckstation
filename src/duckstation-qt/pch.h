ef2086b2c3c2f53c3114eebaa7b83691044ef85c
Stenzek <stenzek@gmail.com>
2025-11-13 22:00:23 +1000

    Qt: Silence new warning from VS2026

--- src/duckstation-qt/pch.h
+++ src/duckstation-qt/pch.h
@@ -5,6 +5,15 @@
 
 #include "core/pch.h"
 
+#ifdef _MSC_VER
+// Disable C4251 outside of Qt code too, because not all headers are included here.
+#pragma warning(disable : 4251) // warning C4251: 'QLayoutItem::align': 'QFlags<Qt::AlignmentFlag>' needs to have
+                                // dll-interface to be used by clients of 'QLayoutItem'
+
+#pragma warning(push)
+#pragma warning(disable : 4864) // warning C4864: expected 'template' keyword before dependent template name
+#endif
+
 #include <QtCore/QByteArray>
 #include <QtCore/QMetaType>
 #include <QtCore/QObject>
@@ -12,3 +21,7 @@
 #include <QtCore/QString>
 #include <QtCore/QtCore>
 #include <QtWidgets/QWidget>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
