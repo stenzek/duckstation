3d27de783520371c4b4a768fad1d625cff893da6
Stenzek <stenzek@gmail.com>
2025-11-29 13:38:15 +1000

    Qt: Fix inverted fullscreen condition

--- src/duckstation-qt/qthost.cpp
+++ src/duckstation-qt/qthost.cpp
@@ -2812,7 +2812,7 @@ void Host::ReleaseRenderWindow()
 bool Host::CanChangeFullscreenMode(bool new_fullscreen_state)
 {
   // Don't mess with fullscreen while locked.
-  return QtHost::IsSystemLocked();
+  return !QtHost::IsSystemLocked();
 }
 
 void EmuThread::updatePerformanceCounters(const GPUBackend* gpu_backend)
