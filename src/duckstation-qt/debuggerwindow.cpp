stenzek/duckstation

commit 9ee1b02cd75d92a7defa3a6f8e4083decc2d4128

Qt: Remove a bunch of unnecessary QSignalBlockers
QAction::setChecked() does not invoke triggered().

src/duckstation-qt/debuggerwindow.cpp
@@ -51,21 +51,14 @@ void DebuggerWindow::onSystemPaused()
   setUIEnabled(true, true);
   refreshAll();
 
-  {
-    QSignalBlocker sb(m_ui.actionPause);
-    m_ui.actionPause->setChecked(true);
-  }
+  m_ui.actionPause->setChecked(true);
 }
 
 void DebuggerWindow::onSystemResumed()
 {
   setUIEnabled(false, true);
   m_ui.codeView->invalidatePC();
-
-  {
-    QSignalBlocker sb(m_ui.actionPause);
-    m_ui.actionPause->setChecked(false);
-  }
+  m_ui.actionPause->setChecked(false);
 }
 
 void DebuggerWindow::onDebuggerMessageReported(const QString& message)
@@ -589,19 +582,10 @@ void DebuggerWindow::setMemoryViewRegion(Bus::MemoryRegion region)
     ((region == Bus::MemoryRegion::RAM) ? static_cast<MemoryViewWidget::EditCallback>(edit_ram_callback) : nullptr);
   m_ui.memoryView->setData(start, mem_ptr, end - start, mem_writable, edit_callback);
 
-#define SET_REGION_RADIO_BUTTON(name, rb_region)                                                                       \
-  do                                                                                                                   \
-  {                                                                                                                    \
-    QSignalBlocker sb(name);                                                                                           \
-    name->setChecked(region == rb_region);                                                                             \
-  } while (0)
-
-  SET_REGION_RADIO_BUTTON(m_ui.memoryRegionRAM, Bus::MemoryRegion::RAM);
-  SET_REGION_RADIO_BUTTON(m_ui.memoryRegionEXP1, Bus::MemoryRegion::EXP1);
-  SET_REGION_RADIO_BUTTON(m_ui.memoryRegionScratchpad, Bus::MemoryRegion::Scratchpad);
-  SET_REGION_RADIO_BUTTON(m_ui.memoryRegionBIOS, Bus::MemoryRegion::BIOS);
-
-#undef SET_REGION_REGION_BUTTON
+  m_ui.memoryRegionRAM->setChecked(region == Bus::MemoryRegion::RAM);
+  m_ui.memoryRegionEXP1->setChecked(region == Bus::MemoryRegion::EXP1);
+  m_ui.memoryRegionScratchpad->setChecked(region == Bus::MemoryRegion::Scratchpad);
+  m_ui.memoryRegionBIOS->setChecked(region == Bus::MemoryRegion::BIOS);
 
   m_ui.memoryView->repaint();
 }

src/duckstation-qt/mainwindow.cpp
@@ -569,12 +569,7 @@ void MainWindow::onSystemStarted()
 
 void MainWindow::onSystemPaused()
 {
-  // update UI
-  {
-    QSignalBlocker sb(m_ui.actionPause);
-    m_ui.actionPause->setChecked(true);
-  }
-
+  m_ui.actionPause->setChecked(true);
   s_system_paused = true;
   updateStatusBarWidgetVisibility();
   m_ui.statusBar->showMessage(tr("Paused"));
@@ -583,12 +578,7 @@ void MainWindow::onSystemPaused()
 
 void MainWindow::onSystemResumed()
 {
-  // update UI
-  {
-    QSignalBlocker sb(m_ui.actionPause);
-    m_ui.actionPause->setChecked(false);
-  }
-
+  m_ui.actionPause->setChecked(false);
   s_system_paused = false;
   m_was_disc_change_request = false;
   m_ui.statusBar->clearMessage();
@@ -600,12 +590,7 @@ void MainWindow::onSystemResumed()
 
 void MainWindow::onSystemStopping()
 {
-  // update UI
-  {
-    QSignalBlocker sb(m_ui.actionPause);
-    m_ui.actionPause->setChecked(false);
-  }
-
+  m_ui.actionPause->setChecked(false);
   s_system_starting = false;
   s_system_valid = false;
   s_system_paused = false;
@@ -654,13 +639,11 @@ void MainWindow::onSystemUndoStateAvailabilityChanged(bool available, quint64 ti
 
 void MainWindow::onMediaCaptureStarted()
 {
-  QSignalBlocker sb(m_ui.actionMediaCapture);
   m_ui.actionMediaCapture->setChecked(true);
 }
 
 void MainWindow::onMediaCaptureStopped()
 {
-  QSignalBlocker sb(m_ui.actionMediaCapture);
   m_ui.actionMediaCapture->setChecked(false);
 }
 
@@ -1405,7 +1388,6 @@ void MainWindow::onPauseActionToggled(bool checked)
       {
         // Restore action state.
         Host::RunOnUIThread([]() {
-          QSignalBlocker sb(g_main_window->m_ui.actionPause);
           g_main_window->m_ui.actionPause->setChecked(false);
         });
       }
@@ -1442,7 +1424,6 @@ void MainWindow::onViewToolbarLockActionToggled(bool checked)
   m_ui.toolBar->setMovable(!checked);
 
   // ensure synced
-  const QSignalBlocker sb(m_ui.actionViewLockToolbar);
   m_ui.actionViewLockToolbar->setChecked(checked);
 }
 
@@ -1453,7 +1434,6 @@ void MainWindow::onViewToolbarSmallIconsActionToggled(bool checked)
   updateToolbarIconStyle();
 
   // ensure synced
-  const QSignalBlocker sb(m_ui.actionViewSmallToolbarIcons);
   m_ui.actionViewSmallToolbarIcons->setChecked(checked);
 }
 
@@ -1464,7 +1444,6 @@ void MainWindow::onViewToolbarLabelsActionToggled(bool checked)
   updateToolbarIconStyle();
 
   // ensure synced
-  const QSignalBlocker sb(m_ui.actionViewToolbarLabels);
   m_ui.actionViewToolbarLabels->setChecked(checked);
 }
 
@@ -1475,7 +1454,6 @@ void MainWindow::onViewToolbarLabelsBesideIconsActionToggled(bool checked)
   updateToolbarIconStyle();
 
   // ensure synced
-  const QSignalBlocker sb(m_ui.actionViewToolbarLabelsBesideIcons);
   m_ui.actionViewToolbarLabelsBesideIcons->setChecked(checked);
 }
 
@@ -1942,10 +1920,7 @@ void MainWindow::onGameListSortIndicatorOrderChanged(int column, Qt::SortOrder o
       activate = (action->data() == column);
 
     if (activate)
-    {
-      const QSignalBlocker sb(m_ui.menuSortBy);
       action->setChecked(true);
-    }
   }
 }
 
@@ -3314,8 +3289,6 @@ void MainWindow::onToolsMediaCaptureToggled(bool checked)
   path = QDir::toNativeSeparators(QFileDialog::getSaveFileName(this, tr("Media Capture"), path, filter));
   if (path.isEmpty())
   {
-    // uncheck it again
-    QSignalBlocker sb(m_ui.actionMediaCapture);
     m_ui.actionMediaCapture->setChecked(false);
     return;
   }

src/duckstation-qt/achievementsettingswidget.cpp
@@ -247,15 +247,11 @@ void AchievementSettingsWidget::onLoginCompleted()
   // Login can enable achievements/hardcore.
   if (!m_ui.enable->isChecked() && Host::GetBaseBoolSettingValue("Cheevos", "Enabled", false))
   {
-    QSignalBlocker sb(m_ui.enable);
     m_ui.enable->setChecked(true);
     updateEnableState();
   }
   if (!m_ui.hardcoreMode->isChecked() && Host::GetBaseBoolSettingValue("Cheevos", "ChallengeMode", false))
-  {
-    QSignalBlocker sb(m_ui.hardcoreMode);
     m_ui.hardcoreMode->setChecked(true);
-  }
 }
 
 void AchievementSettingsWidget::onViewProfilePressed()

src/duckstation-qt/setupwizarddialog.cpp
@@ -636,15 +636,11 @@ void SetupWizardDialog::onAchievementsLoginCompleted()
   // Login can enable achievements/hardcore.
   if (!m_ui.enable->isChecked() && Host::GetBaseBoolSettingValue("Cheevos", "Enabled", false))
   {
-    QSignalBlocker sb(m_ui.enable);
     m_ui.enable->setChecked(true);
     updateAchievementsLoginState();
   }
   if (!m_ui.hardcoreMode->isChecked() && Host::GetBaseBoolSettingValue("Cheevos", "ChallengeMode", false))
-  {
-    QSignalBlocker sb(m_ui.hardcoreMode);
     m_ui.hardcoreMode->setChecked(true);
-  }
 }
 
 void SetupWizardDialog::onAchievementsViewProfileClicked()

commit 65e890f7f4ab55a77cd693eff1deb470e2cac129

Qt: Fix incorrect use of toggled signal
Fixes frame stepping, probably others.

Regression from 9ee1b02cd75d92a7defa3a6f8e4083decc2d4128.

src/duckstation-qt/debuggerwindow.cpp
@@ -92,7 +92,7 @@ void DebuggerWindow::scrollToCodeAddress(VirtualMemoryAddress address, bool cent
   m_ui.codeView->setSelectedAddress(address);
 }
 
-void DebuggerWindow::onPauseActionToggled(bool paused)
+void DebuggerWindow::onPauseActionTriggered(bool paused)
 {
   if (!paused)
   {
@@ -459,7 +459,7 @@ void DebuggerWindow::connectSignals()
   connect(g_emu_thread, &EmuThread::systemDestroyed, this, &DebuggerWindow::onSystemDestroyed);
   connect(g_emu_thread, &EmuThread::debuggerMessageReported, this, &DebuggerWindow::onDebuggerMessageReported);
 
-  connect(m_ui.actionPause, &QAction::toggled, this, &DebuggerWindow::onPauseActionToggled);
+  connect(m_ui.actionPause, &QAction::triggered, this, &DebuggerWindow::onPauseActionTriggered);
   connect(m_ui.actionRunToCursor, &QAction::triggered, this, &DebuggerWindow::onRunToCursorTriggered);
   connect(m_ui.actionGoToPC, &QAction::triggered, this, &DebuggerWindow::onGoToPCTriggered);
   connect(m_ui.actionGoToAddress, &QAction::triggered, this, &DebuggerWindow::onGoToAddressTriggered);

src/duckstation-qt/debuggerwindow.h
@@ -60,7 +60,7 @@ private:
   void timerRefresh();
   void refreshAll();
 
-  void onPauseActionToggled(bool paused);
+  void onPauseActionTriggered(bool paused);
   void onRunToCursorTriggered();
   void onGoToPCTriggered();
   void onGoToAddressTriggered();

src/duckstation-qt/gamecheatsettingswidget.cpp
@@ -200,7 +200,7 @@ GameCheatSettingsWidget::GameCheatSettingsWidget(SettingsWindow* dialog, QWidget
   m_ui.sortCheats->setChecked(sorting_enabled);
 
   connect(m_ui.enableCheats, &QCheckBox::checkStateChanged, this, &GameCheatSettingsWidget::onEnableCheatsChanged);
-  connect(m_ui.sortCheats, &QPushButton::toggled, this, &GameCheatSettingsWidget::onSortCheatsToggled);
+  connect(m_ui.sortCheats, &QPushButton::clicked, this, &GameCheatSettingsWidget::onSortCheatsClicked);
   connect(m_ui.search, &QLineEdit::textChanged, this, &GameCheatSettingsWidget::onSearchFilterChanged);
   connect(m_ui.loadDatabaseCheats, &QCheckBox::checkStateChanged, this,
           &GameCheatSettingsWidget::onLoadDatabaseCheatsChanged);
@@ -271,7 +271,7 @@ void GameCheatSettingsWidget::onEnableCheatsChanged(Qt::CheckState state)
   m_dialog->saveAndReloadGameSettings();
 }
 
-void GameCheatSettingsWidget::onSortCheatsToggled(bool checked)
+void GameCheatSettingsWidget::onSortCheatsClicked(bool checked)
 {
   m_sort_model->sort(checked ? 0 : -1, Qt::AscendingOrder);
 

src/duckstation-qt/gamecheatsettingswidget.h
@@ -45,7 +45,7 @@ public:
 
 private:
   void onEnableCheatsChanged(Qt::CheckState state);
-  void onSortCheatsToggled(bool checked);
+  void onSortCheatsClicked(bool checked);
   void onSearchFilterChanged(const QString& text);
   void onLoadDatabaseCheatsChanged(Qt::CheckState state);
   void onCheatListItemDoubleClicked(const QModelIndex& index);

src/duckstation-qt/mainwindow.cpp
@@ -1371,7 +1371,7 @@ void MainWindow::onResetGameActionTriggered()
   g_emu_thread->resetSystem(true);
 }
 
-void MainWindow::onPauseActionToggled(bool checked)
+void MainWindow::onPauseActionTriggered(bool checked)
 {
   if (s_system_paused == checked)
     return;
@@ -1407,7 +1407,7 @@ void MainWindow::onScanForNewGamesTriggered()
   refreshGameList(false);
 }
 
-void MainWindow::onViewToolbarActionToggled(bool checked)
+void MainWindow::onViewToolbarActionTriggered(bool checked)
 {
   Host::SetBaseBoolSettingValue("UI", "ShowToolbar", checked);
   Host::CommitBaseSettingChanges();
@@ -1415,7 +1415,7 @@ void MainWindow::onViewToolbarActionToggled(bool checked)
   updateToolbarIconStyle();
 }
 
-void MainWindow::onViewToolbarLockActionToggled(bool checked)
+void MainWindow::onViewToolbarLockActionTriggered(bool checked)
 {
   Host::SetBaseBoolSettingValue("UI", "LockToolbar", checked);
   Host::CommitBaseSettingChanges();
@@ -1425,7 +1425,7 @@ void MainWindow::onViewToolbarLockActionToggled(bool checked)
   m_ui.actionViewLockToolbar->setChecked(checked);
 }
 
-void MainWindow::onViewToolbarSmallIconsActionToggled(bool checked)
+void MainWindow::onViewToolbarSmallIconsActionTriggered(bool checked)
 {
   Host::SetBaseBoolSettingValue("UI", "ToolbarSmallIcons", checked);
   Host::CommitBaseSettingChanges();
@@ -1435,7 +1435,7 @@ void MainWindow::onViewToolbarSmallIconsActionToggled(bool checked)
   m_ui.actionViewSmallToolbarIcons->setChecked(checked);
 }
 
-void MainWindow::onViewToolbarLabelsActionToggled(bool checked)
+void MainWindow::onViewToolbarLabelsActionTriggered(bool checked)
 {
   Host::SetBaseBoolSettingValue("UI", "ToolbarLabels", checked);
   Host::CommitBaseSettingChanges();
@@ -1445,7 +1445,7 @@ void MainWindow::onViewToolbarLabelsActionToggled(bool checked)
   m_ui.actionViewToolbarLabels->setChecked(checked);
 }
 
-void MainWindow::onViewToolbarLabelsBesideIconsActionToggled(bool checked)
+void MainWindow::onViewToolbarLabelsBesideIconsActionTriggered(bool checked)
 {
   Host::SetBaseBoolSettingValue("UI", "ToolbarLabelsBesideIcons", checked);
   Host::CommitBaseSettingChanges();
@@ -1455,7 +1455,7 @@ void MainWindow::onViewToolbarLabelsBesideIconsActionToggled(bool checked)
   m_ui.actionViewToolbarLabelsBesideIcons->setChecked(checked);
 }
 
-void MainWindow::onViewStatusBarActionToggled(bool checked)
+void MainWindow::onViewStatusBarActionTriggered(bool checked)
 {
   Host::SetBaseBoolSettingValue("UI", "ShowStatusBar", checked);
   Host::CommitBaseSettingChanges();
@@ -2047,23 +2047,23 @@ void MainWindow::onToolbarContextMenuRequested(const QPoint& pos)
     QAction* action = menu->addAction(tr("Lock Toolbar"));
     action->setCheckable(true);
     action->setChecked(!m_ui.toolBar->isMovable());
-    connect(action, &QAction::toggled, this, &MainWindow::onViewToolbarLockActionToggled);
+    connect(action, &QAction::triggered, this, &MainWindow::onViewToolbarLockActionTriggered);
 
     action = menu->addAction(tr("Small Icons"));
     action->setCheckable(true);
     action->setChecked(Host::GetBaseBoolSettingValue("UI", "ToolbarSmallIcons", false));
-    connect(action, &QAction::toggled, this, &MainWindow::onViewToolbarSmallIconsActionToggled);
+    connect(action, &QAction::triggered, this, &MainWindow::onViewToolbarSmallIconsActionTriggered);
 
     action = menu->addAction(tr("Show Labels"));
     action->setCheckable(true);
     action->setChecked(Host::GetBaseBoolSettingValue("UI", "ToolbarLabels", true));
-    connect(action, &QAction::toggled, this, &MainWindow::onViewToolbarLabelsActionToggled);
+    connect(action, &QAction::triggered, this, &MainWindow::onViewToolbarLabelsActionTriggered);
 
     action = menu->addAction(tr("Labels Beside Icons"));
     action->setCheckable(true);
     action->setChecked(Host::GetBaseBoolSettingValue("UI", "ToolbarLabelsBesideIcons", false));
     action->setEnabled(show_labels);
-    connect(action, &QAction::toggled, this, &MainWindow::onViewToolbarLabelsBesideIconsActionToggled);
+    connect(action, &QAction::triggered, this, &MainWindow::onViewToolbarLabelsBesideIconsActionTriggered);
 
     QMenu* const position_menu = menu->addMenu(tr("Position"));
     QtUtils::StylePopupMenu(position_menu);
@@ -2091,7 +2091,7 @@ void MainWindow::onToolbarContextMenuRequested(const QPoint& pos)
       QAction* const menu_action = menu->addAction((m_ui.*action_ptr)->iconText());
       menu_action->setCheckable(true);
       menu_action->setChecked(StringUtil::IsInStringList(active_buttons, name));
-      connect(menu_action, &QAction::toggled, this, [this, name](bool checked) {
+      connect(menu_action, &QAction::triggered, this, [this, name](bool checked) {
         const std::string active_buttons_str =
           Host::GetBaseStringSettingValue("UI", "ToolbarButtons", DEFAULT_TOOLBAR_ACTIONS);
         std::vector<std::string_view> active_buttons = StringUtil::SplitString(active_buttons_str, ',');
@@ -2417,7 +2417,7 @@ void MainWindow::connectSignals()
   connect(m_ui.actionCloseGameWithoutSaving, &QAction::triggered, this,
           &MainWindow::onCloseGameWithoutSavingActionTriggered);
   connect(m_ui.actionResetGame, &QAction::triggered, this, &MainWindow::onResetGameActionTriggered);
-  connect(m_ui.actionPause, &QAction::toggled, this, &MainWindow::onPauseActionToggled);
+  connect(m_ui.actionPause, &QAction::triggered, this, &MainWindow::onPauseActionTriggered);
   connect(m_ui.actionScreenshot, &QAction::triggered, g_emu_thread, &EmuThread::saveScreenshot);
   connect(m_ui.actionScanForNewGames, &QAction::triggered, this, &MainWindow::onScanForNewGamesTriggered);
   connect(m_ui.actionRescanAllGames, &QAction::triggered, this, [this]() { refreshGameList(true); });
@@ -2444,13 +2444,13 @@ void MainWindow::connectSignals()
   connect(m_ui.actionFolderSettings, &QAction::triggered, [this]() { doSettings("Folders"); });
   connect(m_ui.actionAdvancedSettings, &QAction::triggered, [this]() { doSettings("Advanced"); });
   connect(m_ui.actionControllerProfiles, &QAction::triggered, this, &MainWindow::onSettingsControllerProfilesTriggered);
-  connect(m_ui.actionViewToolbar, &QAction::toggled, this, &MainWindow::onViewToolbarActionToggled);
-  connect(m_ui.actionViewLockToolbar, &QAction::toggled, this, &MainWindow::onViewToolbarLockActionToggled);
-  connect(m_ui.actionViewSmallToolbarIcons, &QAction::toggled, this, &MainWindow::onViewToolbarSmallIconsActionToggled);
-  connect(m_ui.actionViewToolbarLabels, &QAction::toggled, this, &MainWindow::onViewToolbarLabelsActionToggled);
-  connect(m_ui.actionViewToolbarLabelsBesideIcons, &QAction::toggled, this,
-          &MainWindow::onViewToolbarLabelsBesideIconsActionToggled);
-  connect(m_ui.actionViewStatusBar, &QAction::toggled, this, &MainWindow::onViewStatusBarActionToggled);
+  connect(m_ui.actionViewToolbar, &QAction::triggered, this, &MainWindow::onViewToolbarActionTriggered);
+  connect(m_ui.actionViewLockToolbar, &QAction::triggered, this, &MainWindow::onViewToolbarLockActionTriggered);
+  connect(m_ui.actionViewSmallToolbarIcons, &QAction::triggered, this, &MainWindow::onViewToolbarSmallIconsActionTriggered);
+  connect(m_ui.actionViewToolbarLabels, &QAction::triggered, this, &MainWindow::onViewToolbarLabelsActionTriggered);
+  connect(m_ui.actionViewToolbarLabelsBesideIcons, &QAction::triggered, this,
+          &MainWindow::onViewToolbarLabelsBesideIconsActionTriggered);
+  connect(m_ui.actionViewStatusBar, &QAction::triggered, this, &MainWindow::onViewStatusBarActionTriggered);
   connect(m_ui.actionViewGameList, &QAction::triggered, this, &MainWindow::onViewGameListActionTriggered);
   connect(m_ui.actionViewGameGrid, &QAction::triggered, this, &MainWindow::onViewGameGridActionTriggered);
   connect(m_ui.actionViewSystemDisplay, &QAction::triggered, this, &MainWindow::onViewSystemDisplayTriggered);
@@ -2468,7 +2468,7 @@ void MainWindow::connectSignals()
   connect(m_ui.actionISOBrowser, &QAction::triggered, this, &MainWindow::onToolsISOBrowserTriggered);
   connect(m_ui.actionCoverDownloader, &QAction::triggered, this, &MainWindow::onToolsCoverDownloaderTriggered);
   connect(m_ui.actionControllerTest, &QAction::triggered, g_emu_thread, &EmuThread::startControllerTest);
-  connect(m_ui.actionMediaCapture, &QAction::toggled, this, &MainWindow::onToolsMediaCaptureToggled);
+  connect(m_ui.actionMediaCapture, &QAction::triggered, this, &MainWindow::onToolsMediaCaptureTriggered);
   connect(m_ui.actionCaptureGPUFrame, &QAction::triggered, g_emu_thread, &EmuThread::captureGPUFrameDump);
   connect(m_ui.actionCPUDebugger, &QAction::triggered, this, &MainWindow::openCPUDebugger);
   connect(m_ui.actionOpenDataDirectory, &QAction::triggered, this, &MainWindow::onToolsOpenDataDirectoryTriggered);
@@ -3267,7 +3267,7 @@ void MainWindow::onToolsCoverDownloaderTriggered()
   QtUtils::ShowOrRaiseWindow(m_cover_download_window, this, true);
 }
 
-void MainWindow::onToolsMediaCaptureToggled(bool checked)
+void MainWindow::onToolsMediaCaptureTriggered(bool checked)
 {
   if (!s_system_valid)
   {

src/duckstation-qt/mainwindow.h
@@ -272,16 +272,16 @@ private:
   void onCloseGameActionTriggered();
   void onCloseGameWithoutSavingActionTriggered();
   void onResetGameActionTriggered();
-  void onPauseActionToggled(bool checked);
+  void onPauseActionTriggered(bool checked);
   void onFullscreenUIStartedOrStopped(bool running);
   void onRemoveDiscActionTriggered();
   void onScanForNewGamesTriggered();
-  void onViewToolbarActionToggled(bool checked);
-  void onViewToolbarLockActionToggled(bool checked);
-  void onViewToolbarSmallIconsActionToggled(bool checked);
-  void onViewToolbarLabelsActionToggled(bool checked);
-  void onViewToolbarLabelsBesideIconsActionToggled(bool checked);
-  void onViewStatusBarActionToggled(bool checked);
+  void onViewToolbarActionTriggered(bool checked);
+  void onViewToolbarLockActionTriggered(bool checked);
+  void onViewToolbarSmallIconsActionTriggered(bool checked);
+  void onViewToolbarLabelsActionTriggered(bool checked);
+  void onViewToolbarLabelsBesideIconsActionTriggered(bool checked);
+  void onViewStatusBarActionTriggered(bool checked);
   void onViewGameListActionTriggered();
   void onViewGameGridActionTriggered();
   void onViewSystemDisplayTriggered();
@@ -298,7 +298,7 @@ private:
   void onToolsMemoryScannerTriggered();
   void onToolsISOBrowserTriggered();
   void onToolsCoverDownloaderTriggered();
-  void onToolsMediaCaptureToggled(bool checked);
+  void onToolsMediaCaptureTriggered(bool checked);
   void onToolsOpenDataDirectoryTriggered();
   void onToolsOpenTextureDirectoryTriggered();
   void onSettingsTriggeredFromToolbar();

src/duckstation-qt/postprocessingsettingswidget.cpp
@@ -90,8 +90,8 @@ void PostProcessingChainConfigWidget::connectUi()
   connect(m_ui.moveUp, &QPushButton::clicked, this, &PostProcessingChainConfigWidget::onMoveUpButtonClicked);
   connect(m_ui.moveDown, &QPushButton::clicked, this, &PostProcessingChainConfigWidget::onMoveDownButtonClicked);
   connect(m_ui.stages, &QListWidget::itemSelectionChanged, this,
-          &PostProcessingChainConfigWidget::onSelectedShaderChanged);
-  connect(m_ui.stages, &QListWidget::itemChanged, this, &PostProcessingChainConfigWidget::onShaderToggled);
+          &PostProcessingChainConfigWidget::onStageItemSelectionChanged);
+  connect(m_ui.stages, &QListWidget::itemChanged, this, &PostProcessingChainConfigWidget::onStageItemChanged);
 }
 
 std::optional<u32> PostProcessingChainConfigWidget::getSelectedIndex() const
@@ -286,13 +286,13 @@ void PostProcessingChainConfigWidget::onOpenDirectoryButtonClicked()
   QtUtils::OpenURL(this, QUrl::fromLocalFile(QString::fromStdString(EmuFolders::Shaders)));
 }
 
-void PostProcessingChainConfigWidget::onSelectedShaderChanged()
+void PostProcessingChainConfigWidget::onStageItemSelectionChanged()
 {
   std::optional<u32> index = getSelectedIndex();
   updateButtonsAndConfigPane(index);
 }
 
-void PostProcessingChainConfigWidget::onShaderToggled(QListWidgetItem* item)
+void PostProcessingChainConfigWidget::onStageItemChanged(QListWidgetItem* item)
 {
   if (!item)
     return;

src/duckstation-qt/postprocessingsettingswidget.h
@@ -52,8 +52,8 @@ private:
   void onMoveDownButtonClicked();
   void onReloadButtonClicked();
   void onOpenDirectoryButtonClicked();
-  void onSelectedShaderChanged();
-  void onShaderToggled(QListWidgetItem* item);
+  void onStageItemSelectionChanged();
+  void onStageItemChanged(QListWidgetItem* item);
   void triggerSettingsReload();
 
   SettingsWindow* m_dialog;

src/duckstation-qt/qthost.cpp
@@ -1167,13 +1167,11 @@ void EmuThread::resetSystem(bool check_memcard_busy)
   System::ResetSystem();
 }
 
-void EmuThread::setSystemPaused(bool paused, bool wait_until_paused /* = false */)
+void EmuThread::setSystemPaused(bool paused)
 {
   if (!isCurrentThread())
   {
-    QMetaObject::invokeMethod(this, &EmuThread::setSystemPaused,
-                              wait_until_paused ? Qt::BlockingQueuedConnection : Qt::QueuedConnection, paused,
-                              wait_until_paused);
+    QMetaObject::invokeMethod(this, &EmuThread::setSystemPaused, Qt::QueuedConnection, paused);
     return;
   }
 

src/duckstation-qt/qthost.h
@@ -153,7 +153,7 @@ public:
   void resumeSystemFromMostRecentState();
   void shutdownSystem(bool save_state, bool check_memcard_busy);
   void resetSystem(bool check_memcard_busy);
-  void setSystemPaused(bool paused, bool wait_until_paused = false);
+  void setSystemPaused(bool paused);
   void changeDisc(const QString& new_disc_path, bool reset_system, bool check_memcard_busy);
   void changeDiscFromPlaylist(quint32 index);
   void loadState(const QString& path);

src/duckstation-qt/settingwidgetbinder.h
@@ -725,10 +725,10 @@ struct SettingAccessor<QAction>
   template<typename F>
   static void connectValueChanged(QAction* widget, F func)
   {
-    widget->connect(widget, &QAction::toggled, func);
+    widget->connect(widget, &QAction::triggered, func);
   }
 
-  static void disconnect(QAction* widget) { QObject::disconnect(widget, &QAction::toggled, nullptr, nullptr); }
+  static void disconnect(QAction* widget) { QObject::disconnect(widget, &QAction::triggered, nullptr, nullptr); }
 };
 
 /// Binds a widget's value to a setting, updating it when the value changes.