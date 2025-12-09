95a196144c299513522d0d6e69b4e9f8bc41e9e4
Stenzek <stenzek@gmail.com>
2025-12-09 23:21:32 +1000

    Build: Fix RC version field
    It broke once the commit count passed 10,000.

--- src/scmversion/update_rc_version.bat
+++ src/scmversion/update_rc_version.bat
@@ -2,5 +2,5 @@
 
 FOR /F "tokens=* USEBACKQ" %%g IN (`git describe --dirty`) do (SET "TAG=%%g")
 
-powershell -Command "(gc ..\duckstation-qt\duckstation-qt.rc) -replace '1,0,0,1', '"%TAG:~0,1%","%TAG:~2,1%","%TAG:~4,4%",0' | Out-File -encoding ASCII ..\duckstation-qt\duckstation-qt.rc"
-powershell -Command "(gc ..\duckstation-qt\duckstation-qt.rc) -replace '1.0.0.1', '"%TAG:~0,1%"."%TAG:~2,1%"."%TAG:~4,4%"' | Out-File -encoding ASCII ..\duckstation-qt\duckstation-qt.rc"
+powershell -Command "(gc ..\duckstation-qt\duckstation-qt.rc) -replace '1,0,0,1', '"%TAG:~0,1%","%TAG:~2,1%","%TAG:~4,5%",0' | Out-File -encoding ASCII ..\duckstation-qt\duckstation-qt.rc"
+powershell -Command "(gc ..\duckstation-qt\duckstation-qt.rc) -replace '1.0.0.1', '"%TAG:~0,1%"."%TAG:~2,1%"."%TAG:~4,5%"' | Out-File -encoding ASCII ..\duckstation-qt\duckstation-qt.rc"
