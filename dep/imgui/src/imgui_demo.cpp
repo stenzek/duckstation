stenzek/duckstation

commit 09926a37698071a1f7eeac3d3069409d7c61205f

GPUDevice: Update last presented time after present
Instead of before.

Makes present skipping much more effective against NVIDIA's global
framerate cap if enabled, on my system with the cap at 60fps it
went from 120fps to 1400fps. Still about half of the true uncapped
speed, but when the present call blocks for a few milliseconds this
is all you can do. Can't stop presenting frames entirely.

src/util/gpu_device.cpp
@@ -300,8 +300,13 @@ bool GPUSwapChain::ShouldSkipPresentingFrame()
   if (diff < throttle_period)
     return true;
 
-  m_last_frame_displayed_time = now;
   return false;
+}
+
+void GPUSwapChain::UpdateLastFramePresentedTime(u64 presented_time)
+{
+  if (m_allow_present_throttle)
+    m_last_frame_displayed_time = (presented_time != 0) ? presented_time : Timer::GetCurrentValue();
 }
 
 void GPUSwapChain::ThrottlePresentation()

src/util/gpu_device.h
@@ -488,6 +488,7 @@ public:
   virtual bool IsExclusiveFullscreen() const;
 
   bool ShouldSkipPresentingFrame();
+  void UpdateLastFramePresentedTime(u64 presented_time = 0);
   void ThrottlePresentation();
 
   static GSVector4i PreRotateClipRect(WindowInfo::PreRotation prerotation, const GSVector2i surface_size,

src/core/gpu_presenter.cpp
@@ -1275,6 +1275,7 @@ bool GPUPresenter::PresentFrame(GPUPresenter* presenter, GPUBackend* backend, bo
       g_gpu_device->SubmitPresent(swap_chain);
     }
 
+    swap_chain->UpdateLastFramePresentedTime();
     ImGuiManager::NewFrame();
   }
   else

src/util/d3d11_device.cpp
@@ -734,6 +734,7 @@ void D3D11Device::EndPresent(GPUSwapChain* swap_chain, bool explicit_present, u6
   const UINT flags =
     (SC->GetVSyncMode() == GPUVSyncMode::Disabled && SC->IsUsingAllowTearing()) ? DXGI_PRESENT_ALLOW_TEARING : 0;
   SC->GetSwapChain()->Present(sync_interval, flags);
+  SC->UpdateLastFramePresentedTime();
 
   if (m_gpu_timing_enabled)
     StartTimestampQuery();

src/util/d3d12_device.cpp
@@ -1266,6 +1266,7 @@ void D3D12Device::SubmitPresent(GPUSwapChain* swap_chain)
   const UINT flags =
     (SC->GetVSyncMode() == GPUVSyncMode::Disabled && SC->IsUsingAllowTearing()) ? DXGI_PRESENT_ALLOW_TEARING : 0;
   SC->GetSwapChain()->Present(sync_interval, flags);
+  SC->UpdateLastFramePresentedTime();
 }
 
 #ifdef ENABLE_GPU_OBJECT_NAMES

src/util/metal_device.mm
@@ -2592,16 +2592,19 @@ void MetalDevice::EndPresent(GPUSwapChain* swap_chain, bool explicit_present, u6
   EndAnyEncoding();
 
   Timer::Value current_time;
+  Timer::Value presented_time;
   if (present_time != 0 && (current_time = Timer::GetCurrentValue()) < present_time)
   {
     // Need to convert to mach absolute time. Time values should already be in nanoseconds.
     const u64 mach_time_nanoseconds = CocoaTools::ConvertMachTimeBaseToNanoseconds(mach_absolute_time());
     const double mach_present_time = static_cast<double>(mach_time_nanoseconds + (present_time - current_time)) / 1e+9;
     [m_render_cmdbuf presentDrawable:m_layer_drawable atTime:mach_present_time];
+    presented_time = present_time;
   }
   else
   {
     [m_render_cmdbuf presentDrawable:m_layer_drawable];
+    presented_time = 0;
   }
 
   DeferRelease(m_layer_drawable);
@@ -2609,6 +2612,8 @@ void MetalDevice::EndPresent(GPUSwapChain* swap_chain, bool explicit_present, u6
 
   SubmitCommandBuffer();
   TrimTexturePool();
+
+  swap_chain->UpdateLastFramePresentedTime(presented_time);
 }
 
 void MetalDevice::SubmitPresent(GPUSwapChain* swap_chainwel)

src/util/opengl_device.cpp
@@ -803,6 +803,7 @@ void OpenGLDevice::EndPresent(GPUSwapChain* swap_chain, bool explicit_present, u
   }
 
   m_gl_context->SwapBuffers();
+  swap_chain->UpdateLastFramePresentedTime();
 
   if (swap_chain == m_main_swap_chain.get() && m_gpu_timing_enabled)
     StartTimestampQuery();

src/util/vulkan_device.cpp
@@ -1254,6 +1254,7 @@ void VulkanDevice::QueuePresent(VulkanSwapChain* present_swap_chain)
   // submission. Don't care if it fails, we'll deal with that at the presentation call site.
   // Credit to dxvk for the idea.
   present_swap_chain->AcquireNextImage(false);
+  present_swap_chain->UpdateLastFramePresentedTime();
 }
 
 void VulkanDevice::BeginCommandBuffer(u32 index)

commit 2a8256673afe56b4fa4b6e546707945c6d7e6117

GPUDevice: Use separate variables for last/next present time
Fixes erratic frame rates in Big Picture mode.

Regression from 09926a37698071a1f7eeac3d3069409d7c61205f.

dep/imgui/src/imgui_demo.cpp
@@ -3581,7 +3581,7 @@ static void DemoWindowWidgetsText()
             static float custom_size = 16.0f;
             ImGui::SliderFloat("custom_size", &custom_size, 10.0f, 100.0f, "%.0f");
             ImGui::Text("ImGui::PushFont(nullptr, custom_size);");
-            ImGui::PushFont(NULL, custom_size);
+            ImGui::PushFont(NULL, custom_size, -1.0f);
             ImGui::Text("FontSize = %.2f (== %.2f * global_scale)", ImGui::GetFontSize(), custom_size);
             ImGui::PopFont();
 
@@ -3589,14 +3589,14 @@ static void DemoWindowWidgetsText()
             static float custom_scale = 1.0f;
             ImGui::SliderFloat("custom_scale", &custom_scale, 0.5f, 4.0f, "%.2f");
             ImGui::Text("ImGui::PushFont(nullptr, style.FontSizeBase * custom_scale);");
-            ImGui::PushFont(NULL, style.FontSizeBase * custom_scale);
+            ImGui::PushFont(NULL, style.FontSizeBase * custom_scale, -1.0f);
             ImGui::Text("FontSize = %.2f (== style.FontSizeBase * %.2f * global_scale)", ImGui::GetFontSize(), custom_scale);
             ImGui::PopFont();
 
             ImGui::SeparatorText("");
             for (float scaling = 0.5f; scaling <= 4.0f; scaling += 0.5f)
             {
-                ImGui::PushFont(NULL, style.FontSizeBase * scaling);
+                ImGui::PushFont(NULL, style.FontSizeBase * scaling, -1.0f);
                 ImGui::Text("FontSize = %.2f (== style.FontSizeBase * %.2f * global_scale)", ImGui::GetFontSize(), scaling);
                 ImGui::PopFont();
             }

src/core/fullscreenui_widgets.cpp
@@ -1039,6 +1039,14 @@ void FullscreenUI::EndLayout()
   s_state.progress_dialog.Draw();
   s_state.message_dialog.Draw();
 
+#if 0
+  if (HasActiveWindow())
+  {
+    s_state.left_fullscreen_footer_text.append_format("FPS: {:.2f} ({:.3f} ms)", GImGui->IO.Framerate,
+                                                      1000.0f / GImGui->IO.Framerate);
+  }
+#endif
+
   DrawFullscreenFooter();
 
   PopResetLayout();

src/util/gpu_device.cpp
@@ -319,12 +319,12 @@ void GPUSwapChain::ThrottlePresentation()
   // Allow it to fall behind/run ahead up to 2*period. Sleep isn't that precise, plus we need to
   // allow time for the actual rendering.
   const u64 max_variance = sleep_period * 2;
-  if (static_cast<u64>(std::abs(static_cast<s64>(current_ts - m_last_frame_displayed_time))) > max_variance)
-    m_last_frame_displayed_time = current_ts + sleep_period;
+  if (static_cast<u64>(std::abs(static_cast<s64>(current_ts - m_next_throttle_time))) > max_variance)
+    m_next_throttle_time = current_ts + sleep_period;
   else
-    m_last_frame_displayed_time += sleep_period;
+    m_next_throttle_time += sleep_period;
 
-  Timer::SleepUntil(m_last_frame_displayed_time, false);
+  Timer::SleepUntil(m_next_throttle_time, false);
 }
 
 GPUDevice::GPUDevice()

src/util/gpu_device.h
@@ -502,6 +502,7 @@ protected:
   bool m_allow_present_throttle = false;
 
   u64 m_last_frame_displayed_time = 0;
+  u64 m_next_throttle_time = 0;
 };
 
 class GPUDevice