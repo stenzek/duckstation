stenzek/duckstation

commit 5e6a18a584b001982b7f664e4a246f7d509fa39a

VulkanDevice: Wait before starting next command buffer
Potentially avoids a fence wait.

src/util/vulkan_device.cpp
@@ -1158,7 +1158,8 @@ void VulkanDevice::WaitForCommandBufferCompletion(u32 index)
   }
 }
 
-void VulkanDevice::EndAndSubmitCommandBuffer(VulkanSwapChain* present_swap_chain, bool explicit_present)
+void VulkanDevice::EndAndSubmitCommandBuffer(bool wait_for_completion, VulkanSwapChain* present_swap_chain,
+                                             bool explicit_present)
 {
   if (m_device_was_lost) [[unlikely]]
     return;
@@ -1220,10 +1221,31 @@ void VulkanDevice::EndAndSubmitCommandBuffer(VulkanSwapChain* present_swap_chain
     return;
   }
 
-  BeginCommandBuffer((m_current_frame + 1) % NUM_COMMAND_BUFFERS);
-
   if (present_swap_chain && !explicit_present)
     QueuePresent(present_swap_chain);
+
+  // Wait before if the next command buffer has not already been waited for.
+  // Waiting afterwards ends up slightly faster because we can do the resets and such before blocking on the
+  // fence wait, but only if the next buffer is definitely not in use. Otherwise we'll do 2 fence waits.
+  const u32 next_command_buffer_index = (m_current_frame + 1) % NUM_COMMAND_BUFFERS;
+  if (wait_for_completion)
+  {
+    const u32 current_command_buffer_index = m_current_frame;
+    if (m_completed_fence_counter >= m_frame_resources[next_command_buffer_index].fence_counter)
+    {
+      BeginCommandBuffer(next_command_buffer_index);
+      WaitForCommandBufferCompletion(current_command_buffer_index);
+    }
+    else
+    {
+      WaitForCommandBufferCompletion(current_command_buffer_index);
+      BeginCommandBuffer(next_command_buffer_index);
+    }
+  }
+  else
+  {
+    BeginCommandBuffer(next_command_buffer_index);
+  }
 }
 
 void VulkanDevice::QueuePresent(VulkanSwapChain* present_swap_chain)
@@ -1312,11 +1334,7 @@ void VulkanDevice::SubmitCommandBuffer(bool wait_for_completion)
 {
   DebugAssert(!InRenderPass());
 
-  const u32 current_frame = m_current_frame;
-  EndAndSubmitCommandBuffer(nullptr, false);
-
-  if (wait_for_completion)
-    WaitForCommandBufferCompletion(current_frame);
+  EndAndSubmitCommandBuffer(wait_for_completion, nullptr, false);
 
   InvalidateCachedState();
 }
@@ -1901,7 +1919,7 @@ void VulkanDevice::EndPresent(GPUSwapChain* swap_chain, bool explicit_present, u
   VulkanTexture::TransitionSubresourcesToLayout(
     m_current_command_buffer, SC->GetCurrentImage(), GPUTexture::Type::RenderTarget, 0, 1, 0, 1,
     VulkanTexture::Layout::ColorAttachment, VulkanTexture::Layout::PresentSrc);
-  EndAndSubmitCommandBuffer(SC, explicit_present);
+  EndAndSubmitCommandBuffer(false, SC, explicit_present);
   InvalidateCachedState();
   TrimTexturePool();
 }
@@ -2605,7 +2623,7 @@ void VulkanDevice::RenderBlankFrame(VulkanSwapChain* swap_chain)
                                                 0, 1, VulkanTexture::Layout::TransferDst,
                                                 VulkanTexture::Layout::PresentSrc);
 
-  EndAndSubmitCommandBuffer(swap_chain, false);
+  EndAndSubmitCommandBuffer(false, swap_chain, false);
 
   InvalidateCachedState();
 }

src/util/vulkan_device.h
@@ -367,7 +367,7 @@ private:
 
   void BeginCommandBuffer(u32 index);
   void WaitForCommandBufferCompletion(u32 index);
-  void EndAndSubmitCommandBuffer(VulkanSwapChain* present_swap_chain, bool explicit_present);
+  void EndAndSubmitCommandBuffer(bool wait_for_completion, VulkanSwapChain* present_swap_chain, bool explicit_present);
   void QueuePresent(VulkanSwapChain* present_swap_chain);
 
   VkDevice m_device = VK_NULL_HANDLE;

commit c76ee85b1d7e34825e498066b0e2fa4407d39c5a

VulkanDevice: Don't submit during image acquire
Regression from 5e6a18a584b001982b7f664e4a246f7d509fa39a

src/util/vulkan_device.cpp
@@ -1276,7 +1276,7 @@ void VulkanDevice::QueuePresent(VulkanSwapChain* present_swap_chain)
   if (res != VK_SUCCESS)
   {
     VkResult handled_res = res;
-    if (!present_swap_chain->HandleAcquireOrPresentError(handled_res, true))
+    if (!present_swap_chain->HandleAcquireOrPresentError(handled_res))
     {
       LOG_VULKAN_ERROR(res, "vkQueuePresentKHR failed: ");
       return;

src/util/vulkan_swap_chain.cpp
@@ -725,7 +725,7 @@ VkResult VulkanSwapChain::AcquireNextImage(bool handle_errors)
   if (m_image_acquire_result.has_value())
   {
     if (m_image_acquire_result.value() == VK_SUCCESS || !handle_errors ||
-        !HandleAcquireOrPresentError(m_image_acquire_result.value(), false))
+        !HandleAcquireOrPresentError(m_image_acquire_result.value()))
     {
       return m_image_acquire_result.value();
     }
@@ -739,7 +739,7 @@ VkResult VulkanSwapChain::AcquireNextImage(bool handle_errors)
 
   VkResult res = vkAcquireNextImageKHR(VulkanDevice::GetInstance().GetVulkanDevice(), m_swap_chain, UINT64_MAX,
                                        GetImageAcquireSemaphore(), VK_NULL_HANDLE, &m_current_image);
-  if (res != VK_SUCCESS && HandleAcquireOrPresentError(res, false))
+  if (res != VK_SUCCESS && handle_errors && HandleAcquireOrPresentError(res))
   {
     res = vkAcquireNextImageKHR(VulkanDevice::GetInstance().GetVulkanDevice(), m_swap_chain, UINT64_MAX,
                                 GetImageAcquireSemaphore(), VK_NULL_HANDLE, &m_current_image);
@@ -752,23 +752,19 @@ VkResult VulkanSwapChain::AcquireNextImage(bool handle_errors)
   return res;
 }
 
-bool VulkanSwapChain::HandleAcquireOrPresentError(VkResult& res, bool is_present_error)
+bool VulkanSwapChain::HandleAcquireOrPresentError(VkResult& res)
 {
   if (res == VK_SUBOPTIMAL_KHR || res == VK_ERROR_OUT_OF_DATE_KHR)
   {
+    DEV_LOG("Swap chain is {}, recreating swap chain", (res == VK_SUBOPTIMAL_KHR) ? "suboptimal" : "out of date");
+
     VulkanDevice& dev = VulkanDevice::GetInstance();
-    if (is_present_error)
-    {
-      // Older NVIDIA drivers completely lock up if there is no device idle wait prior to waiting of the command
-      // buffer's fences. I'm guessing it's something due to the failed present, but regardless, it shouldn't hurt
-      // anything doing this here. But don't remove it for this reason.
-      vkDeviceWaitIdle(dev.GetVulkanDevice());
-      dev.WaitForAllFences();
-    }
-    else
-    {
-      dev.SubmitCommandBuffer(true);
-    }
+
+    // Older NVIDIA drivers completely lock up if there is no device idle wait prior to waiting of the command
+    // buffer's fences. I'm guessing it's something due to the failed present, but regardless, it shouldn't hurt
+    // anything doing this here. But don't remove it for this reason.
+    vkDeviceWaitIdle(dev.GetVulkanDevice());
+    dev.WaitForAllFences();
 
     Error error;
     if (!RecreateSwapChain(dev, &error))
@@ -783,17 +779,13 @@ bool VulkanSwapChain::HandleAcquireOrPresentError(VkResult& res, bool is_present
   }
   else if (res == VK_ERROR_SURFACE_LOST_KHR)
   {
+    WARNING_LOG("Surface lost, recreating surface");
+
     VulkanDevice& dev = VulkanDevice::GetInstance();
-    if (is_present_error)
-    {
-      // See above.
-      vkDeviceWaitIdle(dev.GetVulkanDevice());
-      dev.WaitForAllFences();
-    }
-    else
-    {
-      dev.SubmitCommandBuffer(true);
-    }
+
+    // See above.
+    vkDeviceWaitIdle(dev.GetVulkanDevice());
+    dev.WaitForAllFences();
 
     Error error;
     if (!RecreateSurface(dev, &error))

src/util/vulkan_swap_chain.h
@@ -52,7 +52,7 @@ public:
   VkResult AcquireNextImage(bool handle_errors);
   void ReleaseCurrentImage();
   void ResetImageAcquireResult();
-  bool HandleAcquireOrPresentError(VkResult& res, bool is_present_error);
+  bool HandleAcquireOrPresentError(VkResult& res);
 
   bool ResizeBuffers(u32 new_width, u32 new_height, Error* error) override;
   bool SetVSyncMode(GPUVSyncMode mode, Error* error) override;