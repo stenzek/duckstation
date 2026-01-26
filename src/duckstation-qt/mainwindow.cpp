38c6c3e769afdf6d9ca5800849fc104548a1f033
Stenzek <stenzek@gmail.com>
2026-01-26 15:45:10 +1000

    Qt: Improve grouping of OSD options

--- src/duckstation-qt/mainwindow.cpp
+++ src/duckstation-qt/mainwindow.cpp
@@ -2458,7 +2458,7 @@ void MainWindow::connectSignals()
           [this]() { doControllerSettings(ControllerSettingsWindow::CATEGORY_GLOBAL_SETTINGS); });
   connect(m_ui.actionMemoryCardSettings, &QAction::triggered, [this]() { doSettings("Memory Cards"); });
   connect(m_ui.actionGraphicsSettings, &QAction::triggered, [this]() { doSettings("Graphics"); });
-  connect(m_ui.actionOSDSettings, &QAction::triggered, [this]() { doSettings("OSD"); });
+  connect(m_ui.actionOSDSettings, &QAction::triggered, [this]() { doSettings("On-Screen Display"); });
   connect(m_ui.actionPostProcessingSettings, &QAction::triggered, [this]() { doSettings("Post-Processing"); });
   connect(m_ui.actionAudioSettings, &QAction::triggered, [this]() { doSettings("Audio"); });
   connect(m_ui.actionAchievementSettings, &QAction::triggered, [this]() { doSettings("Achievements"); });
