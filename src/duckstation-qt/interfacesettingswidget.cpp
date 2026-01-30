stenzek/duckstation

commit 370df62930a7929f26aff6db0a88924bc87ebd61

Qt: Add option to use the system font on Linux

src/duckstation-qt/interfacesettingswidget.cpp
@@ -93,17 +93,22 @@ InterfaceSettingsWidget::InterfaceSettingsWidget(SettingsWindow* dialog, QWidget
   connect(m_ui.hideMainWindow, &QCheckBox::checkStateChanged, this,
           &InterfaceSettingsWidget::updateRenderToSeparateWindowOptions);
 
-#ifdef _WIN32
-  SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.disableWindowRoundedCorners, "Main",
+#if defined(_WIN32)
+  QCheckBox* const disable_window_rounded_corners =
+    new QCheckBox(tr("Disable Window Rounded Corners"), m_ui.appearanceGroup);
+  SettingWidgetBinder::BindWidgetToBoolSetting(sif, disable_window_rounded_corners, "Main",
                                                "DisableWindowRoundedCorners", false);
-#else
-  QtUtils::SafeDeleteWidget(m_ui.disableWindowRoundedCorners);
-#endif
-#ifdef __APPLE__
-  SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.useFractionalWindowScale, "Main", "UseFractionalWindowScale",
+  m_ui.appearanceLayout->addWidget(disable_window_rounded_corners, m_ui.appearanceLayout->rowCount(), 0, 1, 4);
+#elif defined(__APPLE__)
+  QCheckBox* const use_fractional_window_scale = new QCheckBox(tr("Use Fractional Window Scale"), m_ui.appearanceGroup);
+  SettingWidgetBinder::BindWidgetToBoolSetting(sif, use_fractional_window_scale, "Main", "UseFractionalWindowScale",
                                                false);
-#else
-  QtUtils::SafeDeleteWidget(m_ui.useFractionalWindowScale);
+  m_ui.appearanceLayout->addWidget(use_fractional_window_scale, m_ui.appearanceLayout->rowCount(), 0, 1, 4);
+#elif defined(__linux__)
+  QCheckBox* const use_system_font = new QCheckBox(tr("Use System Font"), m_ui.appearanceGroup);
+  SettingWidgetBinder::BindWidgetToBoolSetting(sif, use_system_font, "Main", "UseSystemFont", false);
+  m_ui.appearanceLayout->addWidget(use_system_font, m_ui.appearanceLayout->rowCount(), 0, 1, 4);
+  connect(use_system_font, &QCheckBox::checkStateChanged, this, &QtHost::UpdateApplicationTheme, Qt::QueuedConnection);
 #endif
 
   if (!m_dialog->isPerGameSettings())
@@ -199,13 +204,17 @@ InterfaceSettingsWidget::InterfaceSettingsWidget(SettingsWindow* dialog, QWidget
 
 #if defined(_WIN32)
   dialog->registerWidgetHelp(
-    m_ui.disableWindowRoundedCorners, tr("Disable Window Rounded Corners"), tr("Unchecked"),
+    disable_window_rounded_corners, tr("Disable Window Rounded Corners"), tr("Unchecked"),
     tr(
       "Disables the rounding of windows automatically applied in Windows 11, which may obscure parts of the content."));
 #elif defined(__APPLE__)
   dialog->registerWidgetHelp(
-    m_ui.useFractionalWindowScale, tr("Use Fractional Window Scale"), tr("Unchecked"),
+    use_fractional_window_scale, tr("Use Fractional Window Scale"), tr("Unchecked"),
     tr("Calculates the true scaling factor for your display, avoiding the downsampling applied by MacOS."));
+#elif defined(__linux__)
+  dialog->registerWidgetHelp(use_system_font, tr("Use System Font"), tr("Unchecked"),
+                             tr("Uses the system font for the interface, instead of the bundled Roboto font. Enabling "
+                                "this option may cause some UI elements to not fit within windows."));
 #endif
 
   if (!m_dialog->isPerGameSettings())

src/duckstation-qt/interfacesettingswidget.ui
@@ -158,7 +158,7 @@
      <property name="title">
       <string>Appearance</string>
      </property>
-     <layout class="QGridLayout" name="gridLayout_3" columnstretch="0,1,0,1">
+     <layout class="QGridLayout" name="appearanceLayout" columnstretch="0,1,0,1">
       <item row="0" column="0">
        <widget class="QLabel" name="languageLabel">
         <property name="text">
@@ -178,20 +178,6 @@
       </item>
       <item row="0" column="3">
        <widget class="QComboBox" name="theme"/>
-      </item>
-      <item row="1" column="0" colspan="4">
-       <widget class="QCheckBox" name="disableWindowRoundedCorners">
-        <property name="text">
-         <string>Disable Window Rounded Corners</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="0" colspan="4">
-       <widget class="QCheckBox" name="useFractionalWindowScale">
-        <property name="text">
-         <string>Use Fractional Window Scale</string>
-        </property>
-       </widget>
       </item>
      </layout>
     </widget>

src/duckstation-qt/qthost.cpp
@@ -263,15 +263,6 @@ bool QtHost::EarlyProcessStartup()
     icon_theme_search_paths.push_back(QStringLiteral(":/icons"));
   icon_theme_search_paths.push_back(QStringLiteral(":/standard-icons"));
   QIcon::setThemeSearchPaths(icon_theme_search_paths);
-
-#ifdef __linux__
-  // Fonts on Linux are ugly and too large. This is only part of where we apply it, see qtthemes.cpp for the rest.
-  QFont application_font = QGuiApplication::font();
-  application_font.setFamilies(GetRobotoFontFamilies());
-  application_font.setPixelSize(12);
-  QApplication::setFont(application_font);
-#endif
-
   return true;
 }
 

src/duckstation-qt/qtthemes.cpp
@@ -31,6 +31,12 @@ struct State
   bool is_variable_color_theme = false;
   bool is_dark_theme = false;
   bool unthemed_style_name_set = false;
+
+#ifdef __linux__
+  bool use_system_font = false;
+  bool system_font_set = false;
+  QFont system_font;
+#endif
 };
 } // namespace
 
@@ -56,6 +62,33 @@ void QtHost::UpdateApplicationTheme()
     s_state.unthemed_palette = QApplication::palette();
   }
 
+#ifdef __linux__
+  // Fonts on Linux are ugly and too large. Override it by default.
+  const bool use_system_font = Core::GetBoolSettingValue("Main", "UseSystemFont", false);
+  bool update_font = (use_system_font != s_state.use_system_font);
+  s_state.use_system_font = use_system_font;
+  if (!s_state.system_font_set)
+  {
+    update_font = true;
+    s_state.system_font_set = true;
+    s_state.system_font = QGuiApplication::font();
+  }
+  if (update_font)
+  {
+    if (!use_system_font)
+    {
+      QFont application_font = s_state.system_font;
+      application_font.setFamilies(GetRobotoFontFamilies());
+      application_font.setPixelSize(12);
+      QApplication::setFont(application_font);
+    }
+    else
+    {
+      QApplication::setFont(s_state.system_font);
+    }
+  }
+#endif
+
   SetStyleFromSettings();
   UpdateThemeOnStyleChange();
 }
@@ -77,7 +110,10 @@ void QtHost::SetStyleSheet(const QString& stylesheet)
 #ifdef __linux__
   // Fonts on Linux are ugly and too large. Unfortunately QApplication::setFont() doesn't seem to apply to
   // all widgets, so instead we have to jankily prefix it to all stylesheets.
-  qApp->setStyleSheet(QStringLiteral("QMenu, QMenuBar { font-family: \"Roboto\"; font-size: 12px; }\n") + stylesheet);
+  if (!s_state.use_system_font)
+    qApp->setStyleSheet(QStringLiteral("QMenu, QMenuBar { font-family: \"Roboto\"; font-size: 12px; }\n") + stylesheet);
+  else
+    qApp->setStyleSheet(stylesheet);
 #else
   qApp->setStyleSheet(stylesheet);
 #endif

commit abc4b0b320d3897f7ca3a30bc08c0b1a12034ba6

Qt: Fix crash on Linux when opening game properties
Regression from 370df62930a7929f26aff6db0a88924bc87ebd61

src/duckstation-qt/interfacesettingswidget.cpp
@@ -141,11 +141,8 @@ InterfaceSettingsWidget::InterfaceSettingsWidget(SettingsWindow* dialog, QWidget
     QtUtils::SafeDeleteWidget(m_ui.language);
     QtUtils::SafeDeleteWidget(m_ui.themeLabel);
     QtUtils::SafeDeleteWidget(m_ui.theme);
-
-    // On Linux, we don't have any rounded corner or fractional scaling options.
-#if !defined(_WIN32) && !defined(__APPLE__)
-    QtUtils::SafeDeleteWidget(m_ui.appearanceGroup);
-#endif
+    if (m_ui.appearanceLayout->isEmpty())
+      QtUtils::SafeDeleteWidget(m_ui.appearanceGroup);
 
     delete m_ui.updatesGroup;
     m_ui.autoUpdateTagLabel = nullptr;