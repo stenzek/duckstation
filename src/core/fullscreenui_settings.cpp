stenzek/duckstation

commit 44e055ba0382e228372d6b8ebc22182fba6aea79

Misc: Use delta from initial value for input binding
Avoids sensors falsely tripping.

src/core/fullscreenui_settings.cpp
@@ -454,6 +454,7 @@ void FullscreenUI::InputBindingDialog::Start(SettingsInterface* bsi, InputBindin
     }
 
     const float abs_value = std::abs(value);
+    const float delta_from_initial = std::abs(std::abs(initial_value) - abs_value);
     const bool reverse_threshold = (key.source_subtype == InputSubclass::ControllerAxis &&
                                     std::abs(initial_value) > 0.5f && std::abs(initial_value - min_value) > 0.1f);
 
@@ -462,8 +463,7 @@ void FullscreenUI::InputBindingDialog::Start(SettingsInterface* bsi, InputBindin
       // if this key is in our new binding list, it's a "release", and we're done
       if (other_key.MaskDirection() == key.MaskDirection())
       {
-        // for pedals, we wait for it to go back to near its starting point to commit the binding
-        if ((reverse_threshold ? ((initial_value - value) <= 0.25f) : (abs_value < 0.5f)))
+        if (delta_from_initial <= 0.25f)
         {
           // did we go the full range?
           if (reverse_threshold && initial_value > 0.5f && min_value <= -0.5f)
@@ -488,7 +488,7 @@ void FullscreenUI::InputBindingDialog::Start(SettingsInterface* bsi, InputBindin
     }
 
     // new binding, add it to the list, but wait for a decent distance first, and then wait for release
-    if ((reverse_threshold ? (abs_value < 0.5f) : (abs_value >= 0.5f)))
+    if (delta_from_initial >= 0.5f)
     {
       InputBindingKey key_to_add = key;
       key_to_add.modifier = (value < 0.0f) ? InputModifier::Negate : InputModifier::None;

src/duckstation-qt/inputbindingdialog.cpp
@@ -326,6 +326,7 @@ void InputBindingDialog::inputManagerHookCallback(InputBindingKey key, float val
   logInputEvent(m_bind_type, key, value, initial_value, min_value);
 
   const float abs_value = std::abs(value);
+  const float delta_from_initial = std::abs(std::abs(initial_value) - abs_value);
   const bool reverse_threshold = (key.source_subtype == InputSubclass::ControllerAxis &&
                                   std::abs(initial_value) > 0.5f && std::abs(initial_value - min_value) > 0.1f);
 
@@ -333,8 +334,7 @@ void InputBindingDialog::inputManagerHookCallback(InputBindingKey key, float val
   {
     if (other_key.MaskDirection() == key.MaskDirection())
     {
-      // for pedals, we wait for it to go back to near its starting point to commit the binding
-      if ((reverse_threshold ? ((initial_value - value) <= 0.25f) : (abs_value < 0.5f)))
+      if (delta_from_initial <= 0.25f)
       {
         // did we go the full range?
         if (reverse_threshold && initial_value > 0.5f && min_value <= -0.5f)
@@ -352,7 +352,7 @@ void InputBindingDialog::inputManagerHookCallback(InputBindingKey key, float val
   }
 
   // new binding, add it to the list, but wait for a decent distance first, and then wait for release
-  if ((reverse_threshold ? (abs_value < 0.5f) : (abs_value >= 0.5f)))
+  if (delta_from_initial >= 0.5f)
   {
     InputBindingKey key_to_add = key;
     key_to_add.modifier = (value < 0.0f) ? InputModifier::Negate : InputModifier::None;

src/duckstation-qt/inputbindingwidgets.cpp
@@ -385,6 +385,7 @@ void InputBindingWidget::inputManagerHookCallback(InputBindingKey key, float val
   InputBindingDialog::logInputEvent(m_bind_type, key, value, initial_value, min_value);
 
   const float abs_value = std::abs(value);
+  const float delta_from_initial = std::abs(std::abs(initial_value) - abs_value);
   const bool reverse_threshold = (key.source_subtype == InputSubclass::ControllerAxis &&
                                   std::abs(initial_value) > 0.5f && std::abs(initial_value - min_value) > 0.1f);
 
@@ -392,8 +393,7 @@ void InputBindingWidget::inputManagerHookCallback(InputBindingKey key, float val
   {
     if (other_key.MaskDirection() == key.MaskDirection())
     {
-      // for pedals, we wait for it to go back to near its starting point to commit the binding
-      if ((reverse_threshold ? ((initial_value - value) <= 0.25f) : (abs_value < 0.5f)))
+      if (delta_from_initial <= 0.25f)
       {
         // did we go the full range?
         if (reverse_threshold && initial_value > 0.5f && min_value <= -0.5f)
@@ -411,7 +411,7 @@ void InputBindingWidget::inputManagerHookCallback(InputBindingKey key, float val
   }
 
   // new binding, add it to the list, but wait for a decent distance first, and then wait for release
-  if ((reverse_threshold ? (abs_value < 0.5f) : (abs_value >= 0.5f)))
+  if (delta_from_initial >= 0.5f)
   {
     InputBindingKey key_to_add = key;
     key_to_add.modifier = (value < 0.0f) ? InputModifier::Negate : InputModifier::None;

commit 372e1f3533089389ee042c504ac9fe117b65b02f

InputManager: Fix needing double press for button binding
Regression from 44e055ba0382e228372d6b8ebc22182fba6aea79

src/core/fullscreenui_settings.cpp
@@ -436,8 +436,8 @@ void FullscreenUI::InputBindingDialog::Start(SettingsInterface* bsi, InputBindin
     if (key.source_subtype == InputSubclass::ControllerSensor)
       return InputInterceptHook::CallbackResult::StopProcessingEvent;
 
-    float initial_value = value;
-    float min_value = value;
+    float initial_value;
+    float min_value;
     InputInterceptHook::CallbackResult default_action = InputInterceptHook::CallbackResult::StopProcessingEvent;
     const auto it = std::find_if(m_value_ranges.begin(), m_value_ranges.end(),
                                  [key](const auto& it) { return it.first.bits == key.bits; });
@@ -449,6 +449,8 @@ void FullscreenUI::InputBindingDialog::Start(SettingsInterface* bsi, InputBindin
     }
     else
     {
+      // if this is a button, set an initial value of zero, since we won't get any event for the initial state
+      initial_value = min_value = (key.source_subtype == InputSubclass::ControllerButton) ? 0.0f : value;
       m_value_ranges.emplace_back(key, std::make_pair(initial_value, min_value));
 
       // forward the event to imgui if it's a new key and a release, because this is what triggered the binding to
@@ -456,6 +458,9 @@ void FullscreenUI::InputBindingDialog::Start(SettingsInterface* bsi, InputBindin
       default_action = (value == 0.0f) ? InputInterceptHook::CallbackResult::ContinueProcessingEvent :
                                          InputInterceptHook::CallbackResult::StopProcessingEvent;
     }
+
+    DEV_LOG("Binding input event: key={} value={:.2f} initial_value={:.2f} min_value={:.2f}",
+            InputManager::ConvertInputBindingKeyToString(m_binding_type, key), value, initial_value, min_value);
 
     const float abs_value = std::abs(value);
     const float delta_from_initial = std::abs(std::abs(initial_value) - abs_value);

src/duckstation-qt/inputbindingdialog.cpp
@@ -302,8 +302,8 @@ void InputBindingDialog::inputManagerHookCallback(InputBindingKey key, float val
   if (!isListeningForInput())
     return;
 
-  float initial_value = value;
-  float min_value = value;
+  float initial_value;
+  float min_value;
   auto it = std::find_if(m_value_ranges.begin(), m_value_ranges.end(),
                          [key](const auto& it) { return it.first.bits == key.bits; });
   if (it != m_value_ranges.end())
@@ -313,6 +313,8 @@ void InputBindingDialog::inputManagerHookCallback(InputBindingKey key, float val
   }
   else
   {
+    // if this is a button, set an initial value of zero, since we won't get any event for the initial state
+    initial_value = min_value = (key.source_subtype == InputSubclass::ControllerButton) ? 0.0f : value;
     m_value_ranges.emplace_back(key, std::make_pair(initial_value, min_value));
   }
 

src/duckstation-qt/inputbindingwidgets.cpp
@@ -391,8 +391,8 @@ void InputBindingWidget::inputManagerHookCallback(InputBindingKey key, float val
   if (!isListeningForInput())
     return;
 
-  float initial_value = value;
-  float min_value = value;
+  float initial_value;
+  float min_value;
   auto it = std::find_if(m_value_ranges.begin(), m_value_ranges.end(),
                          [key](const auto& it) { return it.first.bits == key.bits; });
   if (it != m_value_ranges.end())
@@ -402,6 +402,8 @@ void InputBindingWidget::inputManagerHookCallback(InputBindingKey key, float val
   }
   else
   {
+    // if this is a button, set an initial value of zero, since we won't get any event for the initial state
+    initial_value = min_value = (key.source_subtype == InputSubclass::ControllerButton) ? 0.0f : value;
     m_value_ranges.emplace_back(key, std::make_pair(initial_value, min_value));
   }
 