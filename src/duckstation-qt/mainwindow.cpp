stenzek/duckstation

commit ce83fb8f5dc34c1ff750fdef2badcdc94860f2e5

Qt: Hook resume state button up to confirm dialog
Instead of unconditionally loading.

src/duckstation-qt/mainwindow.cpp
@@ -1108,6 +1108,14 @@ bool MainWindow::openResumeStateDialog(const std::string& path, const std::strin
   if (save_state_path.empty() || !FileSystem::FileExists(save_state_path.c_str()))
     return false;
 
+  return openResumeStateDialog(std::move(save_state_path));
+}
+
+bool MainWindow::openResumeStateDialog(std::string save_state_path)
+{
+  if (s_locals.system_valid)
+    return false;
+
   std::optional<ExtendedSaveStateInfo> ssi = System::GetExtendedSaveStateInfo(save_state_path.c_str());
   if (!ssi.has_value())
     return false;
@@ -1169,23 +1177,25 @@ bool MainWindow::openResumeStateDialog(const std::string& path, const std::strin
   QPushButton* const cancel = bbox->addButton(QDialogButtonBox::Cancel);
   load->setDefault(true);
 
-  connect(load, &QPushButton::clicked, [this, dlg, path, save_state_path]() mutable {
+  connect(load, &QPushButton::clicked, [this, dlg, path = ssi->media_path, save_state_path]() mutable {
     startFile(std::move(path), std::move(save_state_path), std::nullopt);
     dlg->accept();
   });
-  connect(boot, &QPushButton::clicked, [this, dlg, path]() mutable {
+  connect(boot, &QPushButton::clicked, [this, dlg, path = ssi->media_path]() mutable {
     startFile(std::move(path), std::nullopt, std::nullopt);
     dlg->accept();
   });
-  connect(delboot, &QPushButton::clicked, [this, dlg, path, save_state_path]() mutable {
-    if (!FileSystem::DeleteFile(save_state_path.c_str()))
-    {
-      QtUtils::MessageBoxCritical(
-        this, tr("Error"), tr("Failed to delete save state file '%1'.").arg(QString::fromStdString(save_state_path)));
-    }
-    startFile(std::move(path), std::nullopt, std::nullopt);
-    dlg->accept();
-  });
+  connect(delboot, &QPushButton::clicked,
+          [this, dlg, path = ssi->media_path, save_state_path = std::move(save_state_path)]() mutable {
+            if (!FileSystem::DeleteFile(save_state_path.c_str()))
+            {
+              QtUtils::MessageBoxCritical(
+                this, tr("Error"),
+                tr("Failed to delete save state file '%1'.").arg(QString::fromStdString(save_state_path)));
+            }
+            startFile(std::move(path), std::nullopt, std::nullopt);
+            dlg->accept();
+          });
   connect(cancel, &QPushButton::clicked, dlg, &QDialog::reject);
 
   main_layout->addWidget(bbox);
@@ -1269,6 +1279,18 @@ void MainWindow::onStartDiscActionTriggered()
 void MainWindow::onStartBIOSActionTriggered()
 {
   g_core_thread->bootSystem(getSystemBootParameters(std::string()));
+}
+
+void MainWindow::onResumeLastStateActionTriggered()
+{
+  std::string state_path = System::GetMostRecentResumeSaveStatePath();
+  if (state_path.empty())
+  {
+    reportError(tr("Error"), tr("No resume save state found."));
+    return;
+  }
+
+  openResumeStateDialog(std::move(state_path));
 }
 
 void MainWindow::onChangeDiscFromFileActionTriggered()
@@ -2424,7 +2446,7 @@ void MainWindow::connectSignals()
   connect(m_ui.actionStartFile, &QAction::triggered, this, &MainWindow::onStartFileActionTriggered);
   connect(m_ui.actionStartDisc, &QAction::triggered, this, &MainWindow::onStartDiscActionTriggered);
   connect(m_ui.actionStartBios, &QAction::triggered, this, &MainWindow::onStartBIOSActionTriggered);
-  connect(m_ui.actionResumeLastState, &QAction::triggered, g_core_thread, &CoreThread::resumeSystemFromMostRecentState);
+  connect(m_ui.actionResumeLastState, &QAction::triggered, this, &MainWindow::onResumeLastStateActionTriggered);
   connect(m_ui.actionChangeDisc, &QAction::triggered, [this] { m_ui.menuChangeDisc->popup(QCursor::pos()); });
   connect(m_ui.actionChangeDiscFromFile, &QAction::triggered, this, &MainWindow::onChangeDiscFromFileActionTriggered);
   connect(m_ui.actionChangeDiscFromDevice, &QAction::triggered, this,

src/duckstation-qt/mainwindow.h
@@ -225,6 +225,7 @@ private:
                                              std::unique_lock<std::recursive_mutex>& lock);
   std::shared_ptr<SystemBootParameters> getSystemBootParameters(std::string file);
   bool openResumeStateDialog(const std::string& path, const std::string& serial);
+  bool openResumeStateDialog(std::string state_path);
   void startFile(std::string path, std::optional<std::string> save_path, std::optional<bool> fast_boot);
   void startFileOrChangeDisc(const QString& qpath);
   void promptForDiscChange(const QString& path);
@@ -262,6 +263,7 @@ private:
   void onStartFileActionTriggered();
   void onStartDiscActionTriggered();
   void onStartBIOSActionTriggered();
+  void onResumeLastStateActionTriggered();
   void onChangeDiscFromFileActionTriggered();
   void onChangeDiscFromGameListActionTriggered();
   void onChangeDiscFromDeviceActionTriggered();

src/duckstation-qt/qthost.cpp
@@ -1125,28 +1125,6 @@ void CoreThread::bootOrLoadState(std::string path)
   }
 }
 
-void CoreThread::resumeSystemFromMostRecentState()
-{
-  if (!isCurrentThread())
-  {
-    QMetaObject::invokeMethod(this, &CoreThread::resumeSystemFromMostRecentState, Qt::QueuedConnection);
-    return;
-  }
-
-  // shouldn't be doing this with a system running
-  if (System::IsValid())
-    return;
-
-  std::string state_filename(System::GetMostRecentResumeSaveStatePath());
-  if (state_filename.empty())
-  {
-    emit errorReported(tr("Error"), tr("No resume save state found."));
-    return;
-  }
-
-  bootOrLoadState(std::move(state_filename));
-}
-
 void CoreThread::onRenderWindowKeyEvent(int key, bool pressed)
 {
   DebugAssert(isCurrentThread());

src/duckstation-qt/qthost.h
@@ -148,7 +148,6 @@ public:
   void exitFullscreenUI();
   void bootSystem(std::shared_ptr<SystemBootParameters> params);
   void bootOrSwitchNonDisc(const QString& path);
-  void resumeSystemFromMostRecentState();
   void shutdownSystem(bool save_state, bool check_safety);
   void resetSystem(bool check_memcard_busy);
   void setSystemPaused(bool paused);

commit 2d727922ed895a6f77e5b9581c2e8cf1ece0bee6

Qt: Allow game to override state media path when resuming
Regression from ce83fb8f5dc34c1ff750fdef2badcdc94860f2e5.

src/duckstation-qt/mainwindow.cpp
@@ -1100,7 +1100,7 @@ std::shared_ptr<SystemBootParameters> MainWindow::getSystemBootParameters(std::s
   return ret;
 }
 
-bool MainWindow::openResumeStateDialog(const std::string& path, const std::string& serial)
+bool MainWindow::openResumeStateDialogForSerial(const std::string& path, const std::string& serial)
 {
   System::FlushSaveStates();
 
@@ -1108,10 +1108,10 @@ bool MainWindow::openResumeStateDialog(const std::string& path, const std::strin
   if (save_state_path.empty() || !FileSystem::FileExists(save_state_path.c_str()))
     return false;
 
-  return openResumeStateDialog(std::move(save_state_path));
+  return openResumeStateDialog(path, std::move(save_state_path));
 }
 
-bool MainWindow::openResumeStateDialog(std::string save_state_path)
+bool MainWindow::openResumeStateDialog(const std::string& path, std::string save_state_path)
 {
   if (s_locals.system_valid)
     return false;
@@ -1169,6 +1169,9 @@ bool MainWindow::openResumeStateDialog(std::string save_state_path)
     }
   }
 
+  // Prefer using the supplied path in case it's been moved.
+  std::string media_path = path.empty() ? std::string(std::move(ssi->media_path)) : std::string(path);
+
   QDialogButtonBox* const bbox = new QDialogButtonBox(Qt::Horizontal, dlg);
 
   QPushButton* const load = bbox->addButton(tr("Load State"), QDialogButtonBox::AcceptRole);
@@ -1177,16 +1180,16 @@ bool MainWindow::openResumeStateDialog(std::string save_state_path)
   QPushButton* const cancel = bbox->addButton(QDialogButtonBox::Cancel);
   load->setDefault(true);
 
-  connect(load, &QPushButton::clicked, [this, dlg, path = ssi->media_path, save_state_path]() mutable {
+  connect(load, &QPushButton::clicked, [this, dlg, path = media_path, save_state_path]() mutable {
     startFile(std::move(path), std::move(save_state_path), std::nullopt);
     dlg->accept();
   });
-  connect(boot, &QPushButton::clicked, [this, dlg, path = ssi->media_path]() mutable {
+  connect(boot, &QPushButton::clicked, [this, dlg, path = media_path]() mutable {
     startFile(std::move(path), std::nullopt, std::nullopt);
     dlg->accept();
   });
   connect(delboot, &QPushButton::clicked,
-          [this, dlg, path = ssi->media_path, save_state_path = std::move(save_state_path)]() mutable {
+          [this, dlg, path = std::move(media_path), save_state_path = std::move(save_state_path)]() mutable {
             if (!FileSystem::DeleteFile(save_state_path.c_str()))
             {
               QtUtils::MessageBoxCritical(
@@ -1227,7 +1230,7 @@ void MainWindow::startFileOrChangeDisc(const QString& qpath)
   {
     const auto lock = GameList::GetLock();
     const GameList::Entry* entry = GameList::GetEntryForPath(path);
-    if (entry && !entry->serial.empty() && openResumeStateDialog(entry->path, entry->serial))
+    if (entry && !entry->serial.empty() && openResumeStateDialogForSerial(entry->path, entry->serial))
       return;
   }
 
@@ -1290,7 +1293,7 @@ void MainWindow::onResumeLastStateActionTriggered()
     return;
   }
 
-  openResumeStateDialog(std::move(state_path));
+  openResumeStateDialog({}, std::move(state_path));
 }
 
 void MainWindow::onChangeDiscFromFileActionTriggered()
@@ -1583,7 +1586,7 @@ void MainWindow::onGameListEntryActivated()
     return;
   }
 
-  if (!entry->serial.empty() && openResumeStateDialog(entry->path, entry->serial))
+  if (!entry->serial.empty() && openResumeStateDialogForSerial(entry->path, entry->serial))
     return;
 
   // only resume if the option is enabled, and we have one for this game

src/duckstation-qt/mainwindow.h
@@ -224,8 +224,8 @@ private:
   const GameList::Entry* resolveDiscSetEntry(const GameList::Entry* entry,
                                              std::unique_lock<std::recursive_mutex>& lock);
   std::shared_ptr<SystemBootParameters> getSystemBootParameters(std::string file);
-  bool openResumeStateDialog(const std::string& path, const std::string& serial);
-  bool openResumeStateDialog(std::string state_path);
+  bool openResumeStateDialogForSerial(const std::string& path, const std::string& serial);
+  bool openResumeStateDialog(const std::string& path, std::string save_state_path);
   void startFile(std::string path, std::optional<std::string> save_path, std::optional<bool> fast_boot);
   void startFileOrChangeDisc(const QString& qpath);
   void promptForDiscChange(const QString& path);