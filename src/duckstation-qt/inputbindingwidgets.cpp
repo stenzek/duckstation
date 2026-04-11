stenzek/duckstation

commit f7f93cc29931b3a2c3c65b08285d6b94312989ba

InputManager: Support localizing device names

src/duckstation-qt/inputbindingwidgets.cpp
@@ -126,12 +126,25 @@ void InputBindingWidget::updateTextAndToolTip()
     setText(tr("%n bindings", nullptr, static_cast<int>(m_bindings.size())));
 
     // keep the full thing for the tooltip
-    const QString qss = QString::fromStdString(StringUtil::JoinString(m_bindings.begin(), m_bindings.end(), "\n"));
-    setToolTip(QStringLiteral("%1\n\n%2\n%3").arg(qss).arg(tr(help_text)).arg(help_clear_text));
+    QString tooltip = tr(help_text);
+    tooltip.append("\n\n"_L1);
+    for (const std::string& binding : m_bindings)
+    {
+      if (SmallString pretty_binding(binding); InputManager::PrettifyInputBinding(pretty_binding, false))
+        tooltip += QtUtils::StringViewToQString(pretty_binding);
+      else
+        tooltip += QString::fromStdString(binding);
+      tooltip += '\n';
+    }
+    tooltip.append(tr(help_clear_text));
+    setToolTip(tooltip);
   }
   else
   {
-    m_full_text = QString::fromStdString(m_bindings[0]);
+    if (SmallString pretty_binding(m_bindings[0]); InputManager::PrettifyInputBinding(pretty_binding, false))
+      m_full_text = QtUtils::StringViewToQString(pretty_binding);
+    else
+      m_full_text = QString::fromStdString(m_bindings[0]);
     updateElidedText();
     setToolTip(QStringLiteral("%1\n\n%2\n%3").arg(m_full_text).arg(tr(help_text)).arg(tr(help_clear_text)));
   }

src/core/fullscreenui_settings.cpp
@@ -330,7 +330,7 @@ void FullscreenUI::DrawInputBindingButton(SettingsInterface* bsi, InputBindingIn
   SmallString value = bsi->GetSmallStringValue(section, name);
   const bool oneline = value.count('&') <= 1;
   if (oneline && type != InputBindingInfo::Type::Pointer && type != InputBindingInfo::Type::Device)
-    InputManager::PrettifyInputBinding(value, &GetControllerIconMapping);
+    InputManager::PrettifyInputBinding(value, true, &GetControllerIconMapping);
 
   if (show_type)
   {

src/core/imgui_overlays.cpp
@@ -1117,7 +1117,7 @@ void SaveStateSelectorUI::DestroyTextures()
 void SaveStateSelectorUI::RefreshHotkeyLegend()
 {
   auto format_legend_entry = [](SmallString binding, std::string_view caption) {
-    InputManager::PrettifyInputBinding(binding, &FullscreenUI::GetControllerIconMapping);
+    InputManager::PrettifyInputBinding(binding, true, &FullscreenUI::GetControllerIconMapping);
     return fmt::format("{} {}", binding, caption);
   };
 

src/duckstation-qt/inputbindingdialog.cpp
@@ -234,8 +234,9 @@ void InputBindingDialog::addNewBinding()
     if (std::find(m_bindings.begin(), m_bindings.end(), new_binding.view()) != m_bindings.end())
       return;
 
-    m_ui.bindingList->addItem(QtUtils::StringViewToQString(new_binding));
     m_bindings.emplace_back(new_binding);
+    InputManager::PrettifyInputBinding(new_binding, false);
+    m_ui.bindingList->addItem(QtUtils::StringViewToQString(new_binding));
     saveListToSettings();
   }
 }
@@ -270,7 +271,11 @@ void InputBindingDialog::updateList()
 {
   m_ui.bindingList->clear();
   for (const std::string& binding : m_bindings)
-    m_ui.bindingList->addItem(QString::fromStdString(binding));
+  {
+    SmallString pretty_binding(binding);
+    InputManager::PrettifyInputBinding(pretty_binding, false);
+    m_ui.bindingList->addItem(QtUtils::StringViewToQString(pretty_binding));
+  }
 }
 
 void InputBindingDialog::saveListToSettings()

src/util/dinput_source.cpp
@@ -449,7 +449,8 @@ TinyString DInputSource::ConvertKeyToString(InputBindingKey key)
   return ret;
 }
 
-TinyString DInputSource::ConvertKeyToIcon(InputBindingKey key, InputManager::BindingIconMappingFunction mapper)
+TinyString DInputSource::ConvertKeyToDisplayString(InputBindingKey key, bool allow_icon,
+                                                   InputManager::BindingIconMappingFunction mapper)
 {
   return {};
 }

src/util/dinput_source.h
@@ -53,7 +53,8 @@ public:
   bool ContainsDevice(std::string_view device) const override;
   std::optional<InputBindingKey> ParseKeyString(std::string_view device, std::string_view binding) override;
   TinyString ConvertKeyToString(InputBindingKey key) override;
-  TinyString ConvertKeyToIcon(InputBindingKey key, InputManager::BindingIconMappingFunction mapper) override;
+  TinyString ConvertKeyToDisplayString(InputBindingKey key, bool allow_icon,
+                                       InputManager::BindingIconMappingFunction mapper) override;
   void SetSubclassPollDeviceList(InputSubclass subclass, const std::span<const InputBindingKey>* devices) override;
   std::unique_ptr<ForceFeedbackDevice> CreateForceFeedbackDevice(std::string_view device, Error* error) override;
 

src/util/input_manager.cpp
@@ -144,8 +144,8 @@ static std::optional<InputBindingKey> ParsePointerKey(std::string_view source, s
 
 static std::vector<std::string_view> SplitChord(std::string_view binding);
 static bool SplitBinding(std::string_view binding, std::string_view* source, std::string_view* sub_binding);
-static void PrettifyInputBindingPart(std::string_view binding, BindingIconMappingFunction mapper, SmallString& ret,
-                                     bool& changed);
+static void PrettifyInputBindingPart(std::string_view binding, bool allow_icon, BindingIconMappingFunction mapper,
+                                     SmallString& ret, bool& changed);
 static void AddBindings(const std::vector<std::string>& bindings, const InputEventHandler& handler);
 static void UpdatePointerCount();
 
@@ -190,10 +190,17 @@ static void UnregisterDeviceNotificationHandle();
 // Tracking host mouse movement and turning into relative events
 // 4 axes: pointer left/right, wheel vertical/horizontal. Last/Next/Normalized.
 // ------------------------------------------------------------------------
-static constexpr const std::array<const char*, static_cast<u8>(InputPointerAxis::Count)> s_pointer_axis_names = {
-  {"X", "Y", "WheelX", "WheelY"}};
-static constexpr const std::array<const char*, 3> s_pointer_button_names = {
-  {"LeftButton", "RightButton", "MiddleButton"}};
+static constexpr const std::array<const char*, static_cast<u8>(InputPointerAxis::Count)> s_pointer_axis_names = {{
+  TRANSLATE_NOOP("InputManager", "X"),
+  TRANSLATE_NOOP("InputManager", "Y"),
+  TRANSLATE_NOOP("InputManager", "WheelX"),
+  TRANSLATE_NOOP("InputManager", "WheelY"),
+}};
+static constexpr const std::array<const char*, 3> s_pointer_button_names = {{
+  TRANSLATE_NOOP("InputManager", "LeftButton"),
+  TRANSLATE_NOOP("InputManager", "RightButton"),
+  TRANSLATE_NOOP("InputManager", "MiddleButton"),
+}};
 
 // ------------------------------------------------------------------------
 // Local Variables
@@ -483,7 +490,8 @@ SmallString InputManager::ConvertInputBindingKeysToString(InputBindingInfo::Type
   return ret;
 }
 
-bool InputManager::PrettifyInputBinding(SmallStringBase& binding, BindingIconMappingFunction mapper /*= nullptr*/)
+bool InputManager::PrettifyInputBinding(SmallStringBase& binding, bool allow_icon,
+                                        BindingIconMappingFunction mapper /*= nullptr*/)
 {
   if (binding.empty())
     return false;
@@ -506,7 +514,7 @@ bool InputManager::PrettifyInputBinding(SmallStringBase& binding, BindingIconMap
       {
         if (!ret.empty())
           ret.append(" + ");
-        PrettifyInputBindingPart(part, mapper, ret, changed);
+        PrettifyInputBindingPart(part, allow_icon, mapper, ret, changed);
       }
     }
     last = next + 1;
@@ -518,7 +526,7 @@ bool InputManager::PrettifyInputBinding(SmallStringBase& binding, BindingIconMap
     {
       if (!ret.empty())
         ret.append(" + ");
-      PrettifyInputBindingPart(part, mapper, ret, changed);
+      PrettifyInputBindingPart(part, allow_icon, mapper, ret, changed);
     }
   }
 
@@ -528,8 +536,8 @@ bool InputManager::PrettifyInputBinding(SmallStringBase& binding, BindingIconMap
   return changed;
 }
 
-void InputManager::PrettifyInputBindingPart(const std::string_view binding, BindingIconMappingFunction mapper,
-                                            SmallString& ret, bool& changed)
+void InputManager::PrettifyInputBindingPart(const std::string_view binding, bool allow_icon,
+                                            BindingIconMappingFunction mapper, SmallString& ret, bool& changed)
 {
   std::string_view source, sub_binding;
   if (!SplitBinding(binding, &source, &sub_binding))
@@ -539,12 +547,24 @@ void InputManager::PrettifyInputBindingPart(const std::string_view binding, Bind
   if (source.starts_with("Keyboard"))
   {
     std::optional<InputBindingKey> key = ParseHostKeyboardKey(source, sub_binding);
-    const char* icon = key.has_value() ? ConvertHostKeyboardCodeToIcon(key->data) : nullptr;
-    if (icon)
+    if (key.has_value())
     {
-      ret.append(icon);
-      changed = true;
-      return;
+      const char* icon = ConvertHostKeyboardCodeToIcon(key->data);
+      if (icon && allow_icon)
+      {
+        ret.append(icon);
+        changed = true;
+        return;
+      }
+      else
+      {
+        if (const char* key_code_string = ConvertHostKeyboardCodeToString(key->data))
+        {
+          ret.append_format(TRANSLATE_FS("InputManager", "Keyboard/{}"), key_code_string);
+          changed = true;
+          return;
+        }
+      }
     }
   }
   else if (source.starts_with("Pointer"))
@@ -558,9 +578,26 @@ void InputManager::PrettifyInputBindingPart(const std::string_view binding, Bind
           ICON_PF_MOUSE_BUTTON_1, ICON_PF_MOUSE_BUTTON_2, ICON_PF_MOUSE_BUTTON_3,
           ICON_PF_MOUSE_BUTTON_4, ICON_PF_MOUSE_BUTTON_5,
         };
-        if (key->data < std::size(button_icons))
+        if (allow_icon && key->data < std::size(button_icons))
         {
           ret.append(button_icons[key->data]);
+          changed = true;
+          return;
+        }
+        else if (key->data < s_pointer_button_names.size())
+        {
+          ret.append_format(TRANSLATE_FS("InputManager", "Pointer-{0}/{1}"), key->source_index,
+                            Host::TranslateToStringView("InputManager", s_pointer_button_names[key->data]));
+          changed = true;
+          return;
+        }
+      }
+      else if (key->source_subtype == InputSubclass::PointerAxis)
+      {
+        if (key->data < s_pointer_axis_names.size())
+        {
+          ret.append_format(TRANSLATE_FS("InputManager", "Pointer-{0}/{1}"), key->source_index,
+                            Host::TranslateToStringView("InputManager", s_pointer_axis_names[key->data]));
           changed = true;
           return;
         }
@@ -579,10 +616,11 @@ void InputManager::PrettifyInputBindingPart(const std::string_view binding, Bind
         std::optional<InputBindingKey> key = s_state.input_sources[i]->ParseKeyString(source, sub_binding);
         if (key.has_value())
         {
-          const TinyString icon = s_state.input_sources[i]->ConvertKeyToIcon(key.value(), mapper);
-          if (!icon.empty())
+          const TinyString display_str =
+            s_state.input_sources[i]->ConvertKeyToDisplayString(key.value(), allow_icon, mapper);
+          if (!display_str.empty())
           {
-            ret.append(icon);
+            ret.append(display_str);
             changed = true;
             return;
           }

src/util/input_manager.h
@@ -250,7 +250,7 @@ SmallString ConvertInputBindingKeysToString(InputBindingInfo::Type binding_type,
 /// Represents a binding with icon fonts, if available.
 /// Optionally maps icon fonts to a different style, e.g. xbox icons -> PS buttons.
 using BindingIconMappingFunction = std::string_view (*)(std::string_view);
-bool PrettifyInputBinding(SmallStringBase& binding, BindingIconMappingFunction mapper = nullptr);
+bool PrettifyInputBinding(SmallStringBase& binding, bool allow_icon, BindingIconMappingFunction mapper = nullptr);
 
 /// Enumerates available devices. Returns a pair of the prefix (e.g. SDL-0) and the device name.
 using DeviceList = std::vector<std::tuple<InputBindingKey, std::string, std::string>>;

commit 3c1689bacdb82ce27cc3ac9263bec9749f11b6a0

Qt: Fix InputBindingWidget tooltip
Ensure the bindings are listed first in all cases, followed by
the help text.

Regression from f7f93cc29931b3a2c3c65b08285d6b94312989ba.

src/duckstation-qt/inputbindingwidgets.cpp
@@ -120,33 +120,30 @@ void InputBindingWidget::updateTextAndToolTip()
     setText(QString());
     setToolTip(QStringLiteral("%1\n\n%2").arg(tr("No binding set.")).arg(tr(help_text)));
   }
-  else if (m_bindings.size() > 1)
+  else
   {
-    m_full_text.clear();
-    setText(tr("%n bindings", nullptr, static_cast<int>(m_bindings.size())));
-
-    // keep the full thing for the tooltip
-    QString tooltip = tr(help_text);
-    tooltip.append("\n\n"_L1);
+    QString bindings_qstr;
     for (const std::string& binding : m_bindings)
     {
       if (SmallString pretty_binding(binding); InputManager::PrettifyInputBinding(pretty_binding, false))
-        tooltip += QtUtils::StringViewToQString(pretty_binding);
+        bindings_qstr += QtUtils::StringViewToQString(pretty_binding);
       else
-        tooltip += QString::fromStdString(binding);
-      tooltip += '\n';
+        bindings_qstr += QString::fromStdString(binding);
+      bindings_qstr += '\n';
     }
-    tooltip.append(tr(help_clear_text));
-    setToolTip(tooltip);
-  }
-  else
-  {
-    if (SmallString pretty_binding(m_bindings[0]); InputManager::PrettifyInputBinding(pretty_binding, false))
-      m_full_text = QtUtils::StringViewToQString(pretty_binding);
+
+    if (m_bindings.size() == 1)
+    {
+      m_full_text = bindings_qstr.trimmed();
+      updateElidedText();
+    }
     else
-      m_full_text = QString::fromStdString(m_bindings[0]);
-    updateElidedText();
-    setToolTip(QStringLiteral("%1\n\n%2\n%3").arg(m_full_text).arg(tr(help_text)).arg(tr(help_clear_text)));
+    {
+      m_full_text.clear();
+      setText(tr("%n bindings", nullptr, static_cast<int>(m_bindings.size())));
+    }
+    // keep the full thing for the tooltip
+    setToolTip(QStringLiteral("%1\n%2\n%3").arg(bindings_qstr).arg(tr(help_text)).arg(tr(help_clear_text)));
   }
 }
 