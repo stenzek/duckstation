stenzek/duckstation

commit 49f5f38c48420eac973e5301679f7455b5e7379a

Qt: Don't use QIcon::fromTheme()
One fewer level of indirection.

src/duckstation-qt/controllerbindingwidgets.cpp
@@ -118,7 +118,7 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_AnalogController ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("controller-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/controller-line.svg"_L1);
     }
     break;
 
@@ -127,7 +127,7 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_AnalogJoystick ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("joystick-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/joystick-line.svg"_L1);
     }
     break;
 
@@ -136,7 +136,7 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_DigitalController ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("controller-digital-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/controller-digital-line.svg"_L1);
     }
     break;
 
@@ -145,7 +145,7 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_GunCon ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("guncon-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/guncon-line.svg"_L1);
     }
     break;
 
@@ -154,7 +154,7 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_NeGcon ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("negcon-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/negcon-line.svg"_L1);
     }
     break;
 
@@ -163,7 +163,7 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_NeGconRumble ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("negcon-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/negcon-rumble-line.svg"_L1);
     }
     break;
 
@@ -172,7 +172,7 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_Mouse ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("mouse-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/mouse-line.svg"_L1);
     }
     break;
 
@@ -181,20 +181,20 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_Justifier ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("guncon-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/guncon-line.svg"_L1);
     }
     break;
 
     case ControllerType::None:
     {
-      m_icon = QIcon::fromTheme("controller-strike-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/controller-strike-line.svg"_L1);
     }
     break;
 
     default:
     {
       createBindingWidgets(m_bindings_widget);
-      m_icon = QIcon::fromTheme("controller-line"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/controller-line.svg"_L1);
     }
     break;
   }
@@ -550,7 +550,7 @@ void ControllerMacroWidget::createWidgets(ControllerBindingWidget* bwidget)
     m_container->addWidget(m_macros[i]);
 
     QListWidgetItem* item = new QListWidgetItem();
-    item->setIcon(QIcon::fromTheme("flashlight-line"_L1));
+    item->setIcon(QIcon(":/icons/monochrome/svg/flashlight-line.svg"_L1));
     m_macroList->addItem(item);
     updateListItem(i);
   }
@@ -961,7 +961,7 @@ ControllerCustomSettingsWidget::ControllerCustomSettingsWidget(ControllerBinding
 
   QHBoxLayout* bottom_hlayout = new QHBoxLayout();
   QPushButton* restore_defaults = new QPushButton(tr("Restore Default Settings"), swidget);
-  restore_defaults->setIcon(QIcon::fromTheme("restart-line"_L1));
+  restore_defaults->setIcon(QIcon(":/icons/monochrome/svg/restart-line.svg"_L1));
   bottom_hlayout->addStretch(1);
   bottom_hlayout->addWidget(restore_defaults);
   swidget_layout->addLayout(bottom_hlayout, current_row++, 0, 1, 4);

src/duckstation-qt/achievementsettingswidget.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>598</width>
-    <height>550</height>
+    <height>552</height>
    </rect>
   </property>
   <layout class="QVBoxLayout" name="verticalLayout">
@@ -66,12 +66,20 @@
         <property name="text">
          <string>View Profile...</string>
         </property>
+        <property name="icon">
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/id-card-line.svg</normaloff>:/icons/monochrome/svg/id-card-line.svg</iconset>
+        </property>
        </widget>
       </item>
       <item>
        <widget class="QPushButton" name="loginButton">
         <property name="text">
          <string>Login...</string>
+        </property>
+        <property name="icon">
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/login-box-line.svg</normaloff>:/icons/monochrome/svg/login-box-line.svg</iconset>
         </property>
        </widget>
       </item>

src/duckstation-qt/advancedsettingswidget.ui
@@ -52,7 +52,8 @@
          <string>Log Channels</string>
         </property>
         <property name="icon">
-         <iconset theme="filter-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/filter-line.svg</normaloff>:/icons/monochrome/svg/filter-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -150,7 +151,8 @@
          <string>Browse...</string>
         </property>
         <property name="icon">
-         <iconset theme="folder-open-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/folder-open-line.svg</normaloff>:/icons/monochrome/svg/folder-open-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -160,7 +162,8 @@
          <string>Open...</string>
         </property>
         <property name="icon">
-         <iconset theme="open-folder-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/open-folder-line.svg</normaloff>:/icons/monochrome/svg/open-folder-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -170,7 +173,8 @@
          <string>Reset</string>
         </property>
         <property name="icon">
-         <iconset theme="delete-back-2-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -199,7 +203,8 @@
          <string>Browse...</string>
         </property>
         <property name="icon">
-         <iconset theme="folder-open-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/folder-open-line.svg</normaloff>:/icons/monochrome/svg/folder-open-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -209,7 +214,8 @@
          <string>Open...</string>
         </property>
         <property name="icon">
-         <iconset theme="open-folder-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/open-folder-line.svg</normaloff>:/icons/monochrome/svg/open-folder-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -219,7 +225,8 @@
          <string>Reset</string>
         </property>
         <property name="icon">
-         <iconset theme="delete-back-2-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -245,7 +252,8 @@
          <string>Refresh</string>
         </property>
         <property name="icon">
-         <iconset theme="refresh-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/refresh-line.svg</normaloff>:/icons/monochrome/svg/refresh-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -255,7 +263,8 @@
          <string>Clear</string>
         </property>
         <property name="icon">
-         <iconset theme="trash-fill"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/trash-fill.svg</normaloff>:/icons/monochrome/svg/trash-fill.svg</iconset>
         </property>
        </widget>
       </item>

src/duckstation-qt/audiosettingswidget.ui
@@ -77,7 +77,8 @@
          <string>Reset Output Volume</string>
         </property>
         <property name="icon">
-         <iconset theme="delete-back-2-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -129,7 +130,8 @@
          <string>Reset Fast Forward Volume</string>
         </property>
         <property name="icon">
-         <iconset theme="delete-back-2-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -271,7 +273,8 @@
            <string>Reset Buffer Size</string>
           </property>
           <property name="icon">
-           <iconset theme="delete-back-2-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -397,7 +400,8 @@
          <string>Reset Sequence Length</string>
         </property>
         <property name="icon">
-         <iconset theme="delete-back-2-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -452,7 +456,8 @@
          <string>Reset Seek Window</string>
         </property>
         <property name="icon">
-         <iconset theme="delete-back-2-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -507,7 +512,8 @@
          <string>Reset Overlap</string>
         </property>
         <property name="icon">
-         <iconset theme="delete-back-2-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
         </property>
        </widget>
       </item>

src/duckstation-qt/biossettingswidget.cpp
@@ -182,7 +182,7 @@ void BIOSSettingsWidget::setDropDownValue(QComboBox* cb, const std::optional<std
     }
   }
 
-  cb->addItem(QIcon::fromTheme("close-line"_L1), tr("%1 (Missing)").arg(qname), QVariant(qname));
+  cb->addItem(QIcon(":/icons/monochrome/svg/close-line.png"_L1), tr("%1 (Missing)").arg(qname), QVariant(qname));
   cb->setCurrentIndex(cb->count() - 1);
 }
 

src/duckstation-qt/biossettingswidget.ui
@@ -111,7 +111,8 @@
          <string>Refresh BIOS List</string>
         </property>
         <property name="icon">
-         <iconset theme="refresh-line"/>
+         <iconset resource="resources/duckstation-qt.qrc">
+          <normaloff>:/icons/monochrome/svg/refresh-line.svg</normaloff>:/icons/monochrome/svg/refresh-line.svg</iconset>
         </property>
        </widget>
       </item>
@@ -125,6 +126,10 @@
           <property name="text">
            <string>Browse...</string>
           </property>
+          <property name="icon">
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/folder-open-line.svg</normaloff>:/icons/monochrome/svg/folder-open-line.svg</iconset>
+          </property>
          </widget>
         </item>
         <item>
@@ -132,12 +137,20 @@
           <property name="text">
            <string>Open...</string>
           </property>
+          <property name="icon">
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/open-folder-line.svg</normaloff>:/icons/monochrome/svg/open-folder-line.svg</iconset>
+          </property>
          </widget>
         </item>
         <item>
          <widget class="QPushButton" name="searchDirectoryReset">
           <property name="text">
            <string>Reset</string>
+          </property>
+          <property name="icon">
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -238,6 +251,8 @@
    </item>
   </layout>
  </widget>
- <resources/>
+ <resources>
+  <include location="resources/duckstation-qt.qrc"/>
+ </resources>
  <connections/>
 </ui>

src/duckstation-qt/capturesettingswidget.ui
@@ -97,7 +97,8 @@
            <string>Browse...</string>
           </property>
           <property name="icon">
-           <iconset theme="folder-open-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/folder-open-line.svg</normaloff>:/icons/monochrome/svg/folder-open-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -107,7 +108,8 @@
            <string>Open...</string>
           </property>
           <property name="icon">
-           <iconset theme="open-folder-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/open-folder-line.svg</normaloff>:/icons/monochrome/svg/open-folder-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -117,7 +119,8 @@
            <string>Reset</string>
           </property>
           <property name="icon">
-           <iconset theme="delete-back-2-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -180,7 +183,8 @@
            <string>Browse...</string>
           </property>
           <property name="icon">
-           <iconset theme="folder-open-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/folder-open-line.svg</normaloff>:/icons/monochrome/svg/folder-open-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -190,7 +194,8 @@
            <string>Open...</string>
           </property>
           <property name="icon">
-           <iconset theme="open-folder-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/open-folder-line.svg</normaloff>:/icons/monochrome/svg/open-folder-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -200,7 +205,8 @@
            <string>Reset</string>
           </property>
           <property name="icon">
-           <iconset theme="delete-back-2-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</normaloff>:/icons/monochrome/svg/delete-back-2-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -433,6 +439,8 @@
    </item>
   </layout>
  </widget>
- <resources/>
+ <resources>
+  <include location="resources/duckstation-qt.qrc"/>
+ </resources>
  <connections/>
 </ui>

src/duckstation-qt/controllerbindingwidget.ui
@@ -10,7 +10,7 @@
     <height>617</height>
    </rect>
   </property>
-  <layout class="QVBoxLayout" name="verticalLayout" stretch="0,1">
+  <layout class="QVBoxLayout" name="verticalLayout" stretch="0,0">
    <property name="leftMargin">
     <number>0</number>
    </property>
@@ -40,7 +40,8 @@
            <string>Bindings</string>
           </property>
           <property name="icon">
-           <iconset theme="controller-digital-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/controller-digital-line.svg</normaloff>:/icons/monochrome/svg/controller-digital-line.svg</iconset>
           </property>
           <property name="checkable">
            <bool>true</bool>
@@ -59,7 +60,8 @@
            <string>Settings</string>
           </property>
           <property name="icon">
-           <iconset theme="checkbox-multiple-blank-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/black/svg/checkbox-multiple-blank-line.svg</normaloff>:/icons/black/svg/checkbox-multiple-blank-line.svg</iconset>
           </property>
           <property name="checkable">
            <bool>true</bool>
@@ -78,7 +80,8 @@
            <string>Macros</string>
           </property>
           <property name="icon">
-           <iconset theme="flashlight-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/flashlight-line.svg</normaloff>:/icons/monochrome/svg/flashlight-line.svg</iconset>
           </property>
           <property name="checkable">
            <bool>true</bool>
@@ -114,7 +117,8 @@
            <string>Automatic Mapping</string>
           </property>
           <property name="icon">
-           <iconset theme="controller-digital-line"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/controller-digital-line.svg</normaloff>:/icons/monochrome/svg/controller-digital-line.svg</iconset>
           </property>
          </widget>
         </item>
@@ -124,7 +128,8 @@
            <string>Clear Mapping</string>
           </property>
           <property name="icon">
-           <iconset theme="trash-fill"/>
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/trash-fill.svg</normaloff>:/icons/monochrome/svg/trash-fill.svg</iconset>
           </property>
          </widget>
         </item>
@@ -139,7 +144,7 @@
   </layout>
  </widget>
  <resources>
-  <include location="../resources/resources.qrc"/>
+  <include location="resources/duckstation-qt.qrc"/>
  </resources>
  <connections/>
 </ui>

commit 5ad980b3223ea26a72983d11dacc4a9cf2bdacb0

Qt: Fix incorrect icon path
Regression from 49f5f38c48420eac973e5301679f7455b5e7379a.

src/duckstation-qt/controllerbindingwidgets.cpp
@@ -169,7 +169,7 @@ void ControllerBindingWidget::populateWidgets()
       Ui::ControllerBindingWidget_NeGconRumble ui;
       ui.setupUi(m_bindings_widget);
       bindBindingWidgets(m_bindings_widget);
-      m_icon = QIcon(":/icons/monochrome/svg/negcon-rumble-line.svg"_L1);
+      m_icon = QIcon(":/icons/monochrome/svg/negcon-line.svg"_L1);
       image_widget = ui.image;
     }
     break;