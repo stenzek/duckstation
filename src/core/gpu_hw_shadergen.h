5ebeff9c23af1d4d9b7ab7761cd3b329c0b806d0
Stenzek <stenzek@gmail.com>
2026-06-24 18:48:50 +1000

    GPU/HW: Use UIDs for shader generation

--- src/core/gpu_hw_shadergen.h
+++ src/core/gpu_hw_shadergen.h
@@ -10,22 +10,55 @@
 class GPU_HW_ShaderGen : public ShaderGen
 {
 public:
+  struct BatchVertexShaderSelector
+  {
+    bool upscaled : 1;
+    bool msaa : 1;
+    bool per_sample_shading : 1;
+    bool textured : 1;
+    bool palette : 1;
+    bool page_texture : 1;
+    bool uv_limits : 1;
+    bool force_round_texcoords : 1;
+    bool pgxp_depth : 1;
+    bool disable_color_perspective : 1;
+  };
+
+  struct BatchFragmentShaderSelector
+  {
+    GPU_HW::BatchRenderMode render_mode;
+    GPUTransparencyMode transparency;
+    GPU_HW::BatchTextureMode texture_mode;
+    GPUTextureFilter texture_filtering;
+    bool is_blended_texture_filtering;
+    bool upscaled : 1;
+    bool msaa : 1;
+    bool per_sample_shading : 1;
+    bool uv_limits : 1;
+    bool force_round_texcoords : 1;
+    bool modulation_crop : 1;
+    bool true_color : 1;
+    bool dithering : 1;
+    bool scaled_dithering : 1;
+    bool disable_color_perspective : 1;
+    bool interlacing : 1;
+    bool scaled_interlacing : 1;
+    bool check_mask : 1;
+    bool write_mask_as_depth : 1;
+    bool use_rov : 1;
+    bool use_rov_depth : 1;
+    bool rov_depth_test : 1;
+    bool rov_depth_write : 1;
+  };
+
+public:
   GPU_HW_ShaderGen(RenderAPI render_api, bool supports_dual_source_blend, bool supports_framebuffer_fetch);
   ~GPU_HW_ShaderGen();
 
   std::string GenerateScreenVertexShader() const;
 
-  std::string GenerateBatchVertexShader(bool upscaled, bool msaa, bool per_sample_shading, bool textured, bool palette,
-                                        bool page_texture, bool uv_limits, bool force_round_texcoords, bool pgxp_depth,
-                                        bool disable_color_perspective) const;
-  std::string GenerateBatchFragmentShader(GPU_HW::BatchRenderMode render_mode, GPUTransparencyMode transparency,
-                                          GPU_HW::BatchTextureMode texture_mode, GPUTextureFilter texture_filtering,
-                                          bool is_blended_texture_filtering, bool upscaled, bool msaa,
-                                          bool per_sample_shading, bool uv_limits, bool force_round_texcoords,
-                                          bool modulation_crop, bool true_color, bool dithering, bool scaled_dithering,
-                                          bool disable_color_perspective, bool interlacing, bool scaled_interlacing,
-                                          bool check_mask, bool write_mask_as_depth, bool use_rov, bool use_rov_depth,
-                                          bool rov_depth_test, bool rov_depth_write) const;
+  std::string GenerateBatchVertexShader(const BatchVertexShaderSelector sel) const;
+  std::string GenerateBatchFragmentShader(const BatchFragmentShaderSelector sel) const;
   std::string GenerateWireframeGeometryShader() const;
   std::string GenerateWireframeFragmentShader() const;
   std::string GenerateVRAMReadFragmentShader(u32 resolution_scale, u32 multisamples) const;
