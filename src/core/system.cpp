stenzek/duckstation

commit 9e4ef8bd69b3d4ebcd0fc07f0dc53e936b20a049

System: Only use fast forward volume for >1 speed

src/core/system.cpp
@@ -5329,7 +5329,9 @@ bool System::IsFastForwardingBoot()
 
 u8 System::GetAudioOutputVolume()
 {
-  return g_settings.GetAudioOutputVolume(IsRunningAtNonStandardSpeed());
+  return ((s_state.target_speed == 0.0 || s_state.target_speed > 1.0f) && !s_state.syncing_to_host) ?
+           g_settings.audio_fast_forward_volume :
+           g_settings.audio_output_volume;
 }
 
 void System::UpdateVolume()

src/core/settings.h
@@ -414,11 +414,6 @@ struct Settings : public GPUSettings
 
   ALWAYS_INLINE bool IsRunaheadEnabled() const { return (runahead_frames > 0); }
 
-  ALWAYS_INLINE u8 GetAudioOutputVolume(bool fast_forwarding) const
-  {
-    return audio_output_muted ? 0 : (fast_forwarding ? audio_fast_forward_volume : audio_output_volume);
-  }
-
   /// Returns the default type for the specified port.
   ALWAYS_INLINE static ControllerType GetDefaultControllerType(u32 pad)
   {

commit f1e08937459c463f586bdb9cd9c1c9ad55aeb262

System: Fix mute volume
Regression from 9e4ef8bd69b3d4ebcd0fc07f0dc53e936b20a049.

src/core/system.cpp
@@ -5329,9 +5329,11 @@ bool System::IsFastForwardingBoot()
 
 u8 System::GetAudioOutputVolume()
 {
-  return ((s_state.target_speed == 0.0 || s_state.target_speed > 1.0f) && !s_state.syncing_to_host) ?
-           g_settings.audio_fast_forward_volume :
-           g_settings.audio_output_volume;
+  return g_settings.audio_output_muted ?
+           0 :
+           (((s_state.target_speed == 0.0 || s_state.target_speed > 1.0f) && !s_state.syncing_to_host) ?
+              g_settings.audio_fast_forward_volume :
+              g_settings.audio_output_volume);
 }
 
 void System::UpdateVolume()