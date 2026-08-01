stenzek/duckstation

commit 8dbc4760693e960c3facb7ca787fe701b3bc25bc

Qt: Add BIOS Installer and improve BIOS setup page
Hopefully make it easier for users...

src/duckstation-qt/biossettingswidget.cpp
@@ -10,8 +10,13 @@
 #include "core/bios.h"
 #include "core/settings.h"
 
+#include "common/error.h"
+#include "common/file_system.h"
+#include "common/path.h"
+
 #include <QtCore/QDir>
 #include <QtWidgets/QFileDialog>
+#include <QtWidgets/QMessageBox>
 #include <algorithm>
 
 #include "moc_biossettingswidget.cpp"
@@ -72,6 +77,10 @@ BIOSSettingsWidget::BIOSSettingsWidget(SettingsWindow* dialog, QWidget* parent) 
     }
   });
 
+  connect(m_ui.install, &QPushButton::clicked, this, [this]() {
+    if (installBIOS(this))
+      refreshList();
+  });
   connect(m_ui.rescan, &QPushButton::clicked, this, &BIOSSettingsWidget::refreshList);
 
   if (!m_dialog->isPerGameSettings())
@@ -95,6 +104,11 @@ BIOSSettingsWidget::BIOSSettingsWidget(SettingsWindow* dialog, QWidget* parent) 
 
   refreshList();
 
+  dialog->registerWidgetHelp(m_ui.rescan, tr("Refresh List"), QString(),
+                             tr("Refreshes the list of BIOS images that can be selected."));
+  dialog->registerWidgetHelp(m_ui.install, tr("Install BIOS"), QString(),
+                             tr("Installs a new BIOS image into the emulator's BIOS directory."));
+
   dialog->registerWidgetHelp(m_ui.pioDeviceType, tr("Device Type"), tr("None"),
                              tr("Simulates a device plugged into the console's parallel port. Usually these are flash "
                                 "cartridges, and require some sort of image dump to function."));
@@ -112,6 +126,57 @@ BIOSSettingsWidget::BIOSSettingsWidget(SettingsWindow* dialog, QWidget* parent) 
 }
 
 BIOSSettingsWidget::~BIOSSettingsWidget() = default;
+
+bool BIOSSettingsWidget::installBIOS(QWidget* parent)
+{
+  const QString source_path = QDir::toNativeSeparators(QFileDialog::getOpenFileName(parent, tr("Select BIOS Image")));
+  if (source_path.isEmpty())
+    return false;
+
+  const std::string source_path_str(source_path.toStdString());
+  Error error;
+  const std::optional<BIOS::Image> image = BIOS::LoadImageFromFile(source_path_str.c_str(), &error);
+  if (!image.has_value())
+  {
+    QtUtils::AsyncMessageBox(parent, QMessageBox::Critical, tr("BIOS Install Error"),
+                             QString::fromStdString(error.GetDescription()));
+    return false;
+  }
+
+  const std::string_view source_filename = Path::GetFileName(source_path_str);
+  const std::string_view destination_filename =
+    (image->info && image->info->expected_filename) ? image->info->expected_filename : source_filename;
+  const std::string destination_path = Path::Combine(EmuFolders::Bios, destination_filename);
+
+  bool replace = false;
+  if (FileSystem::FileExists(destination_path.c_str()))
+  {
+    if (QtUtils::MessageBoxQuestion(
+          parent, tr("BIOS Already Installed"),
+          tr("The BIOS file '%1' is already installed. Do you want to overwrite it?")
+            .arg(QString::fromUtf8(destination_filename.data(), static_cast<qsizetype>(destination_filename.size()))),
+          QMessageBox::Yes | QMessageBox::No) != QMessageBox::Yes)
+    {
+      return false;
+    }
+
+    replace = true;
+  }
+
+  if (!FileSystem::CopyFilePath(source_path_str.c_str(), destination_path.c_str(), replace, &error))
+  {
+    QtUtils::AsyncMessageBox(parent, QMessageBox::Critical, tr("BIOS Install Error"),
+                             QString::fromStdString(error.GetDescription()));
+    return false;
+  }
+
+  QMessageBox::information(
+    parent, tr("BIOS Installed"),
+    tr("BIOS '%1' installed as '%2'.")
+      .arg(image->info ? QString::fromUtf8(image->info->description) : tr("Unknown"),
+           QString::fromUtf8(destination_filename.data(), static_cast<qsizetype>(destination_filename.size()))));
+  return true;
+}
 
 void BIOSSettingsWidget::refreshList()
 {

src/core/fullscreenui_settings.cpp
@@ -109,6 +109,7 @@ static void SaveCustomSerial(const std::string& path, const std::string& serial)
 static void DrawInterfaceSettingsPage();
 static void DrawGameListSettingsPage();
 static void DrawBIOSSettingsPage();
+static void InstallBIOS(std::string source_path, bool replace);
 static void DrawConsoleSettingsPage();
 static void DrawEmulationSettingsPage();
 static void DrawGraphicsSettingsPage();
@@ -3179,6 +3180,15 @@ void FullscreenUI::DrawBIOSSettingsPage()
 
   MenuHeading(FSUI_VSTR("Options"));
 
+  if (MenuButton(FSUI_ICONVSTR(ICON_FA_FILE_IMPORT, "Install BIOS"),
+                 FSUI_VSTR("Copies a BIOS image to the configured global BIOS directory.")))
+  {
+    OpenFileSelector(FSUI_ICONVSTR(ICON_FA_FILE_IMPORT, "Select BIOS Image"), {}, {}, [](std::string path) {
+      if (!path.empty())
+        InstallBIOS(std::move(path), false);
+    });
+  }
+
   DrawFolderSetting(bsi, FSUI_ICONVSTR(ICON_FA_FOLDER, "BIOS Directory"), "BIOS", "SearchDirectory", EmuFolders::Bios);
 
   DrawToggleSetting(bsi, FSUI_ICONVSTR(ICON_FA_SCROLL, "Enable TTY Logging"),
@@ -3186,6 +3196,47 @@ void FullscreenUI::DrawBIOSSettingsPage()
                     "TTYLogging", false);
 
   EndMenuButtons();
+}
+
+void FullscreenUI::InstallBIOS(std::string source_path, bool replace)
+{
+  Error error;
+  const std::optional<BIOS::Image> image = BIOS::LoadImageFromFile(source_path.c_str(), &error);
+  if (!image.has_value())
+  {
+    OpenInfoMessageDialog(ICON_EMOJI_NO_ENTRY_SIGN, FSUI_STR("BIOS Install Error"), error.TakeDescription());
+    return;
+  }
+
+  const std::string_view source_filename = Path::GetFileName(source_path);
+  const std::string_view destination_filename =
+    (image->info && image->info->expected_filename) ? image->info->expected_filename : source_filename;
+  const std::string destination_path = Path::Combine(EmuFolders::Bios, destination_filename);
+
+  if (!replace && FileSystem::FileExists(destination_path.c_str()))
+  {
+    OpenConfirmMessageDialog(
+      ICON_EMOJI_WARNING, FSUI_STR("BIOS Already Installed"),
+      fmt::format(FSUI_FSTR("The BIOS file '{}' is already installed. Do you want to overwrite it?"),
+                  destination_filename),
+      [source_path = std::move(source_path)](bool result) mutable {
+        if (result)
+          InstallBIOS(std::move(source_path), true);
+      });
+    return;
+  }
+
+  // Copy the whole thing (PS2 BIOS only loads the first 512KB)
+  if (!FileSystem::CopyFilePath(source_path.c_str(), destination_path.c_str(), replace, &error))
+  {
+    OpenInfoMessageDialog(ICON_EMOJI_NO_ENTRY_SIGN, FSUI_STR("BIOS Install Error"), error.TakeDescription());
+    return;
+  }
+
+  OpenInfoMessageDialog(ICON_EMOJI_CHECKMARK_BUTTON, FSUI_STR("BIOS Installed"),
+                        fmt::format(FSUI_FSTR("BIOS '{}' installed as '{}'."),
+                                    image->info ? image->info->description : FSUI_VSTR("Unknown"),
+                                    destination_filename));
 }
 
 void FullscreenUI::DrawConsoleSettingsPage()

src/core/fullscreenui_strings.h
@@ -119,7 +119,11 @@ TRANSLATE_NOOP("FullscreenUI", "Automatically saves the system state when closin
 TRANSLATE_NOOP("FullscreenUI", "Automatically switches to fullscreen mode when the program is started.");
 TRANSLATE_NOOP("FullscreenUI", "Automatically switches to the next disc in the game when the game stops the CD-ROM motor. Does not work for all games.");
 TRANSLATE_NOOP("FullscreenUI", "Avoids calls to C++ code, significantly speeding up the recompiler.");
+TRANSLATE_NOOP("FullscreenUI", "BIOS '{}' installed as '{}'.");
+TRANSLATE_NOOP("FullscreenUI", "BIOS Already Installed");
 TRANSLATE_NOOP("FullscreenUI", "BIOS Directory");
+TRANSLATE_NOOP("FullscreenUI", "BIOS Install Error");
+TRANSLATE_NOOP("FullscreenUI", "BIOS Installed");
 TRANSLATE_NOOP("FullscreenUI", "BIOS Selection");
 TRANSLATE_NOOP("FullscreenUI", "BIOS Settings");
 TRANSLATE_NOOP("FullscreenUI", "BIOS for {}");
@@ -187,6 +191,7 @@ TRANSLATE_NOOP("FullscreenUI", "Controls");
 TRANSLATE_NOOP("FullscreenUI", "Controls how dithering is applied in the emulated GPU. True Color disables dithering and produces the nicest looking gradients.");
 TRANSLATE_NOOP("FullscreenUI", "Controls the volume of the audio played on the host when fast forwarding.");
 TRANSLATE_NOOP("FullscreenUI", "Controls the volume of the audio played on the host.");
+TRANSLATE_NOOP("FullscreenUI", "Copies a BIOS image to the configured global BIOS directory.");
 TRANSLATE_NOOP("FullscreenUI", "Copies the current global settings to this game.");
 TRANSLATE_NOOP("FullscreenUI", "Copies the global controller configuration to this game.");
 TRANSLATE_NOOP("FullscreenUI", "Copy Global Settings");
@@ -447,6 +452,7 @@ TRANSLATE_NOOP("FullscreenUI", "Indicator Scale");
 TRANSLATE_NOOP("FullscreenUI", "Informational Message Duration");
 TRANSLATE_NOOP("FullscreenUI", "Inhibit Screensaver");
 TRANSLATE_NOOP("FullscreenUI", "Input Sources");
+TRANSLATE_NOOP("FullscreenUI", "Install BIOS");
 TRANSLATE_NOOP("FullscreenUI", "Interface Settings");
 TRANSLATE_NOOP("FullscreenUI", "Internal Resolution");
 TRANSLATE_NOOP("FullscreenUI", "Language");
@@ -694,6 +700,7 @@ TRANSLATE_NOOP("FullscreenUI", "Search Directories");
 TRANSLATE_NOOP("FullscreenUI", "Search on redump.info");
 TRANSLATE_NOOP("FullscreenUI", "Seek Speedup");
 TRANSLATE_NOOP("FullscreenUI", "Select");
+TRANSLATE_NOOP("FullscreenUI", "Select BIOS Image");
 TRANSLATE_NOOP("FullscreenUI", "Select Device");
 TRANSLATE_NOOP("FullscreenUI", "Select Disc");
 TRANSLATE_NOOP("FullscreenUI", "Select Disc Drive");
@@ -826,6 +833,7 @@ TRANSLATE_NOOP("FullscreenUI", "Texture Filtering");
 TRANSLATE_NOOP("FullscreenUI", "Texture Modulation Cropping (\"Old/v0\" GPU)");
 TRANSLATE_NOOP("FullscreenUI", "Texture Replacements");
 TRANSLATE_NOOP("FullscreenUI", "Textures Directory");
+TRANSLATE_NOOP("FullscreenUI", "The BIOS file '{}' is already installed. Do you want to overwrite it?");
 TRANSLATE_NOOP("FullscreenUI", "The SDL input source supports most controllers.");
 TRANSLATE_NOOP("FullscreenUI", "The audio backend determines how frames produced by the emulator are submitted to the host.");
 TRANSLATE_NOOP("FullscreenUI", "The selected memory card image will be used in shared mode for this slot.");

src/duckstation-qt/biossettingswidget.h
@@ -22,6 +22,7 @@ public:
   explicit BIOSSettingsWidget(SettingsWindow* dialog, QWidget* parent);
   ~BIOSSettingsWidget();
 
+  static bool installBIOS(QWidget* parent);
   static void populateDropDownForRegion(ConsoleRegion region, QComboBox* cb,
                                         std::vector<std::pair<std::string, const BIOS::ImageInfo*>>& images,
                                         bool per_game);

src/duckstation-qt/biossettingswidget.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>732</width>
-    <height>470</height>
+    <height>472</height>
    </rect>
   </property>
   <layout class="QVBoxLayout" name="mainLayout">
@@ -28,7 +28,7 @@
      <property name="title">
       <string>BIOS Selection</string>
      </property>
-     <layout class="QGridLayout" name="gridLayout">
+     <layout class="QGridLayout" name="gridLayout" columnminimumwidth="175,0">
       <item row="0" column="0">
        <widget class="QLabel" name="label">
         <property name="text">
@@ -46,6 +46,13 @@
         </property>
        </widget>
       </item>
+      <item row="1" column="0">
+       <widget class="QLabel" name="label_2">
+        <property name="text">
+         <string>NTSC-U/C (US/Canada):</string>
+        </property>
+       </widget>
+      </item>
       <item row="1" column="1">
        <widget class="QComboBox" name="imageNTSCU">
         <property name="sizePolicy">
@@ -53,13 +60,6 @@
           <horstretch>0</horstretch>
           <verstretch>0</verstretch>
          </sizepolicy>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="0">
-       <widget class="QLabel" name="label_2">
-        <property name="text">
-         <string>NTSC-U/C (US/Canada):</string>
         </property>
        </widget>
       </item>
@@ -100,21 +100,30 @@
        </widget>
       </item>
       <item row="0" column="2">
-       <widget class="QPushButton" name="rescan">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Maximum" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="toolTip">
-         <string>Refresh BIOS List</string>
-        </property>
-        <property name="icon">
-         <iconset resource="resources/duckstation-qt.qrc">
-          <normaloff>:/icons/monochrome/svg/refresh-line.svg</normaloff>:/icons/monochrome/svg/refresh-line.svg</iconset>
-        </property>
-       </widget>
+       <layout class="QHBoxLayout" name="horizontalLayout">
+        <item>
+         <widget class="QPushButton" name="install">
+          <property name="text">
+           <string>Install BIOS</string>
+          </property>
+          <property name="icon">
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/file-add-line.svg</normaloff>:/icons/monochrome/svg/file-add-line.svg</iconset>
+          </property>
+         </widget>
+        </item>
+        <item>
+         <widget class="QPushButton" name="rescan">
+          <property name="text">
+           <string>Refresh List</string>
+          </property>
+          <property name="icon">
+           <iconset resource="resources/duckstation-qt.qrc">
+            <normaloff>:/icons/monochrome/svg/refresh-line.svg</normaloff>:/icons/monochrome/svg/refresh-line.svg</iconset>
+          </property>
+         </widget>
+        </item>
+       </layout>
       </item>
       <item row="1" column="0" colspan="3">
        <layout class="QHBoxLayout" name="directoryGroupBoxHorizontalLayout">

src/duckstation-qt/gamesummarywidget.cpp
@@ -626,12 +626,12 @@ void GameSummaryWidget::processHashResults(const GameDatabase::TrackVerification
     if (verification.track_matches[i])
     {
       brush = QColor(0, 200, 0);
-      row->setText(5, QString::fromUtf8(u8"\u2713"));
+      row->setText(5, u"\u2713"_s);
     }
     else
     {
       brush = QColor(200, 0, 0);
-      row->setText(5, QString::fromUtf8(u8"\u2715"));
+      row->setText(5, u"\u2715"_s);
     }
     row->setForeground(4, brush);
     row->setForeground(5, brush);

src/duckstation-qt/setupwizarddialog.cpp
@@ -226,6 +226,10 @@ void SetupWizardDialog::setupBIOSPage()
   refreshBiosList();
 
   connect(m_ui.biosSearchDirectory, &QLineEdit::textChanged, this, &SetupWizardDialog::refreshBiosList);
+  connect(m_ui.installBios, &QPushButton::clicked, this, [this]() {
+    if (BIOSSettingsWidget::installBIOS(this))
+      refreshBiosList();
+  });
   connect(m_ui.refreshBiosList, &QPushButton::clicked, this, &SetupWizardDialog::refreshBiosList);
 }
 
@@ -239,6 +243,24 @@ void SetupWizardDialog::refreshBiosList()
   BIOSSettingsWidget::setDropDownValue(m_ui.imageNTSCU, Core::GetBaseStringSettingValue("BIOS", "PathNTSCU"), false);
   BIOSSettingsWidget::setDropDownValue(m_ui.imageNTSCJ, Core::GetBaseStringSettingValue("BIOS", "PathNTSCJ"), false);
   BIOSSettingsWidget::setDropDownValue(m_ui.imagePAL, Core::GetBaseStringSettingValue("BIOS", "PathPAL"), false);
+
+  if (list.empty())
+  {
+    QPalette palette = m_ui.biosSummaryIcon->palette();
+    palette.setBrush(QPalette::WindowText, QBrush(QColor(200, 0, 0)));
+    m_ui.biosSummaryIcon->setPalette(palette);
+    m_ui.biosSummaryIcon->setText(u"\u2715"_s);
+    m_ui.biosSummary->setText(tr("No BIOS images found. Please install one to continue."));
+  }
+  else
+  {
+    QPalette palette = m_ui.biosSummaryIcon->palette();
+    palette.setBrush(QPalette::WindowText, QBrush(QColor(0, 200, 0)));
+    m_ui.biosSummaryIcon->setPalette(palette);
+    m_ui.biosSummaryIcon->setText(u"\u2713"_s);
+    m_ui.biosSummary->setText(tr("%n BIOS images found. You can install additional BIOS images if desired.",
+                                 "BIOS Count", static_cast<int>(list.size())));
+  }
 }
 
 void SetupWizardDialog::setupGameListPage(bool initial)

commit 07401690b9e9e6cc9f5985b7f22795c60724951f

Qt: Fix crash opening game settings
Regression from 8dbc4760693e960c3facb7ca787fe701b3bc25bc.

src/duckstation-qt/biossettingswidget.cpp
@@ -94,6 +94,8 @@ BIOSSettingsWidget::BIOSSettingsWidget(SettingsWindow* dialog, QWidget* parent) 
   {
     m_ui.mainLayout->removeWidget(m_ui.directoryGroupBox);
     delete m_ui.directoryGroupBox;
+    m_ui.rescan = nullptr;
+    m_ui.install = nullptr;
     m_ui.directoryGroupBox = nullptr;
     m_ui.directoryGroupBoxLabel = nullptr;
     m_ui.directoryGroupBoxLayout = nullptr;